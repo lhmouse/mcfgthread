b25b9b1801417f0861a43482e37af52ec82bb8c4
LIU Hao <lh_mouse@126.com>
2022-04-05 18:52:02 +0800

    once: Remove an unused macro

--- src/once.h
+++ src/once.h
@@ -22,12 +22,10 @@ extern "C" {
 struct __MCF_once
   {
     uintptr_t __ready : 8;  // this conforms to the Itanium C++ ABI
-
     uintptr_t __locked : 1;
 
-#define __MCF_ONCE_NS_B   (__INTPTR_WIDTH__ - 9)
-#define __MCF_ONCE_NS_M    (UINTPTR_MAX    >> 9)
-    uintptr_t __nsleep : __MCF_ONCE_NS_B;    // number of sleeping threads
+    uintptr_t __nsleep : __INTPTR_WIDTH__ - 9;  // number of sleeping threads
+#define __MCF_ONCE_NS_M  (UINTPTR_MAX >> 9)
   };
 
 // Initializes a once-initialization flag dynamically.
