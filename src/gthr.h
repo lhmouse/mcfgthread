38745a65d13e369104d5d17ff851cd918570a45c
LIU Hao <lh_mouse@126.com>
2022-04-11 09:13:21 +0800

    gthr: Eliminate type-punning warning with GCC 5

--- src/gthr.h
+++ src/gthr.h
@@ -563,10 +563,12 @@ __MCF_gthr_join(__gthread_t __thrd, void** __resultp_opt) __MCF_NOEXCEPT
     if(__resultp_opt) {
       // As there is no type information, we examine the thread procedure to
       // ensure we don't mistake a thread of a wrong type.
-      if(__thrd->__proc != __MCF_gthr_thread_thunk)
-        *__resultp_opt = NULL;
+      if(__thrd->__proc == __MCF_gthr_thread_thunk) {
+        __MCF_gthr_thread_record* __rec = (__MCF_gthr_thread_record*) __thrd->__data;
+        *__resultp_opt = __rec->__result;
+      }
       else
-        *__resultp_opt = ((__MCF_gthr_thread_record*) __thrd->__data)->__result;
+        *__resultp_opt = NULL;
     }
 
     // Free the thread.
