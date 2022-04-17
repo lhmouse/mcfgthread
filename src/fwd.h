ad692b76947c0c6ed16fbb3f98cc7510245a9de3
LIU Hao <lh_mouse@126.com>
2022-04-17 17:15:04 +0800

    startup: Don't detach the current thread upon process exit

--- src/fwd.h
+++ src/fwd.h
@@ -94,14 +94,9 @@ uint32_t
 _MCF_get_win32_error(void) __MCF_NOEXCEPT
   __attribute__((__pure__));
 
-// These functions are declared here for the sake of completeness, and are not
-// meant to be call directly by users.
-void
-__MCF_initialize(void) __MCF_NOEXCEPT;
-
-void
-__MCF_finalize_on_thread_exit(void) __MCF_NOEXCEPT;
-
+// Performs process cleanup. This function is called by `__cxa_finalize()`. It
+// is declared here for the sake of completeness, and is not meant to be called
+// directly.
 void
 __MCF_finalize_on_exit(void) __MCF_NOEXCEPT;
 
