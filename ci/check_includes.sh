6720977779cb4ba366d8709b3c686b54765858ae
LIU Hao <lh_mouse@126.com>
2022-04-18 22:10:29 +0800

    ci/check_includes: Fix message

--- ci/check_includes.sh
+++ ci/check_includes.sh
@@ -7,18 +7,18 @@ export CPPFLAGS="-D_FILE_OFFSET_BITS=64 -D_POSIX_C_SOURCE=200809 -D_GNU_SOURCE -
 
 for _file in $(find -L "src" -name "*.h")
 do
-  echo "Checking headers as C89:  ${_cmd}  \"${_file}\""
+  echo "Checking header as C89:  ${_cmd}  \"${_file}\""
   ${CC} ${CPPFLAGS} ${CFLAGS} -x c -std=c99 -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
 done
 
 for _file in $(find -L "src" -name "*.h")
 do
-  echo "Checking headers as C++:  ${_cmd}  \"${_file}\""
+  echo "Checking header as C++:  ${_cmd}  \"${_file}\""
   ${CC} ${CPPFLAGS} ${CFLAGS} -x c++ -std=c++98 -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
 done
 
 for _file in $(find -L "src" -name "*.c")
 do
-  echo "Checking sources as C99:  ${_cmd}  \"${_file}\""
+  echo "Checking source as C99:  ${_cmd}  \"${_file}\""
   ${CC} ${CPPFLAGS} ${CFLAGS} -x c -std=c99 -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
 done
