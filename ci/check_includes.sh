39e5a4511351923c5dd39faa3d04850d2f7684ce
LIU Hao <lh_mouse@126.com>
2022-04-22 12:36:05 +0800

    ci/check_includes: Update

--- ci/check_includes.sh
+++ ci/check_includes.sh
@@ -3,22 +3,28 @@
 # setup
 export CC=${CC:-"gcc"}
 export CXX=${CXX:-"g++"}
-export CPPFLAGS="-D_FILE_OFFSET_BITS=64 -D_POSIX_C_SOURCE=200809 -D_GNU_SOURCE -D_WIN32_WINNT=0x0601"
+export CPPFLAGS="-D_WIN32_WINNT=0x0601 -Wall -Wextra"
 
 for _file in $(find -L "src" -name "*.h")
 do
   echo "Checking header as C89:  ${_cmd}  \"${_file}\""
-  ${CC} ${CPPFLAGS} ${CFLAGS} -x c -std=c99 -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
+  ${CC} ${CPPFLAGS} ${CFLAGS} -x c -std=c89 -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
 done
 
 for _file in $(find -L "src" -name "*.h")
 do
-  echo "Checking header as C++:  ${_cmd}  \"${_file}\""
+  echo "Checking header as C++98:  ${_cmd}  \"${_file}\""
   ${CC} ${CPPFLAGS} ${CFLAGS} -x c++ -std=c++98 -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
+done
+
+for _file in $(find -L "src" -name "*.h")
+do
+  echo "Checking header as C++11:  ${_cmd}  \"${_file}\""
+  ${CC} ${CPPFLAGS} ${CFLAGS} -x c++ -std=c++11 -Wpedantic -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
 done
 
 for _file in $(find -L "src" -name "*.c")
 do
   echo "Checking source as C99:  ${_cmd}  \"${_file}\""
-  ${CC} ${CPPFLAGS} ${CFLAGS} -x c -std=c99 -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
+  ${CC} ${CPPFLAGS} ${CFLAGS} -x c -std=c99 -Wpedantic -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
 done
