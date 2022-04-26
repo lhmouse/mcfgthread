171b1d4f989854fb9f496695b4e10dd9da2638b2
LIU Hao <lh_mouse@126.com>
2022-04-26 22:13:14 +0800

    startup: Shorten a line

--- src/startup.c
+++ src/startup.c
@@ -50,8 +50,9 @@ do_startup_crt_initialize(PVOID instance)
     __MCF_perf_frequency_reciprocal = 1000 / (double) freq.QuadPart;
 
     /* Attach the main thread.  */
-    __MCF_main_thread.__tid = GetCurrentThreadId();
-    __MCF_main_thread.__handle = OpenThread(THREAD_ALL_ACCESS, FALSE, __MCF_main_thread.__tid);
+    DWORD main_tid = GetCurrentThreadId();
+    __MCF_main_thread.__tid = main_tid;
+    __MCF_main_thread.__handle = OpenThread(THREAD_ALL_ACCESS, FALSE, main_tid);
     __MCFGTHREAD_CHECK(__MCF_main_thread.__handle);
     __MCF_ATOMIC_STORE_REL(__MCF_main_thread.__nref, 1);
     __MCFGTHREAD_CHECK(TlsSetValue(__MCF_win32_tls_index, &__MCF_main_thread));
