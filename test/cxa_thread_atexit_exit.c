a74e0e8a72b340bbc1bb84d2ffc400cee6ad14f1
LIU Hao <lh_mouse@126.com>
2022-05-01 21:52:56 +0800

    test/cxa_thread_atexit_exit: New

---
+++ test/cxa_thread_atexit_exit.c
@@ -0,0 +1,24 @@
+/* This file is part of MCF Gthread.
+ * See LICENSE.TXT for licensing information.
+ * Copyleft 2022, LH_Mouse. All wrongs reserved.  */
+
+#include "../src/cxa.h"
+#include "../src/exit.h"
+#include <assert.h>
+#include <stdio.h>
+
+static
+void
+tls_dtor(void* ptr)
+  {
+    __MCF__Exit(*(int*) ptr);
+  }
+
+int
+main(void)
+  {
+    static int value = 0;
+    __MCF_cxa_thread_atexit(tls_dtor, &value, NULL);
+
+    __MCF_exit(42);
+  }
