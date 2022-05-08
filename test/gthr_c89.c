ce28107eed8c6d2a268118349b4c1b6abdb3aec0
LIU Hao <lh_mouse@126.com>
2022-05-08 13:20:48 +0800

    test/gthr_c89: New test

---
+++ test/gthr_c89.c
@@ -0,0 +1,15 @@
+/* This file is part of MCF Gthread.
+ * See LICENSE.TXT for licensing information.
+ * Copyleft 2022, LH_Mouse. All wrongs reserved.  */
+
+#include "../src/gthr.h"
+
+#if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+#  error Please compile this file as C89.
+#endif
+
+int
+main(void)
+  {
+    return 0;
+  }
