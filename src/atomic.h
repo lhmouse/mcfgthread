abbdee6d06db781d72c1874117b6a6afa611f3a4
LIU Hao <lh_mouse@126.com>
2022-05-11 23:29:31 +0800

    atomic: Simplify macros

--- src/atomic.h
+++ src/atomic.h
@@ -19,12 +19,12 @@
  * The value that has been read is returned as an integer.
  *
  * `INTEGER _MCF_atomic_load_WIDTH_ORDER(const void* mem);`  */
-#define __MCF_ATOMIC_PROTOTYPE_(INTEGER, SUFFIX)  \
+#define __MCF_ATOMIC_FUNCTION_(INTEGER, SUFFIX, ORDER_SUCC, ORDER_FAIL)  \
   INTEGER  \
-  _MCF_atomic_load_##SUFFIX(const void* __mem) __MCF_NOEXCEPT
-
-#define __MCF_ATOMIC_STATEMENTS_(INTEGER, ORDER_SUCC, ORDER_FAIL)  \
-    return __atomic_load_n((const INTEGER*) __mem, ORDER_SUCC);
+  _MCF_atomic_load_##SUFFIX(const void* __mem) __MCF_NOEXCEPT  \
+    {  \
+      return __atomic_load_n((const INTEGER*) __mem, ORDER_SUCC);  \
+    }
 
 #define __MCF_ATOMIC_HAS_ACQUIRE_
 #include "xatomic.i"
@@ -33,13 +33,13 @@
  * The value that has been read is returned via the pointer `retp`.
  *
  * `void _MCF_atomic_load_pWIDTH_ORDER(void* retp, const void* mem);`  */
-#define __MCF_ATOMIC_PROTOTYPE_(INTEGER, SUFFIX)  \
+#define __MCF_ATOMIC_FUNCTION_(INTEGER, SUFFIX, ORDER_SUCC, ORDER_FAIL)  \
   void  \
-  _MCF_atomic_load_p##SUFFIX(void* __retp, const void* __mem) __MCF_NOEXCEPT
-
-#define __MCF_ATOMIC_STATEMENTS_(INTEGER, ORDER_SUCC, ORDER_FAIL)  \
-    INTEGER __val = __atomic_load_n((const INTEGER*) __mem, ORDER_SUCC);  \
-    *(INTEGER*) __retp = __val;
+  _MCF_atomic_load_p##SUFFIX(void* __retp, const void* __mem) __MCF_NOEXCEPT  \
+    {  \
+      INTEGER __val = __atomic_load_n((const INTEGER*) __mem, ORDER_SUCC);  \
+      *(INTEGER*) __retp = __val;  \
+    }
 
 #define __MCF_ATOMIC_HAS_ACQUIRE_
 #include "xatomic.i"
@@ -48,13 +48,13 @@
  * The value to write is passed by value as an integer.
  *
  * `INTEGER _MCF_atomic_store_WIDTH_ORDER(void* mem, INTEGER val);`  */
-#define __MCF_ATOMIC_PROTOTYPE_(INTEGER, SUFFIX)  \
+#define __MCF_ATOMIC_FUNCTION_(INTEGER, SUFFIX, ORDER_SUCC, ORDER_FAIL)  \
   INTEGER  \
-  _MCF_atomic_store_##SUFFIX(void* __mem, INTEGER __val) __MCF_NOEXCEPT
-
-#define __MCF_ATOMIC_STATEMENTS_(INTEGER, ORDER_SUCC, ORDER_FAIL)  \
-    __atomic_store_n((INTEGER*) __mem, __val, ORDER_SUCC);  \
-    return __val;
+  _MCF_atomic_store_##SUFFIX(void* __mem, INTEGER __val) __MCF_NOEXCEPT  \
+    {  \
+      __atomic_store_n((INTEGER*) __mem, __val, ORDER_SUCC);  \
+      return __val;  \
+    }
 
 #define __MCF_ATOMIC_HAS_RELEASE_
 #include "xatomic.i"
@@ -63,13 +63,13 @@
  * The value to write is passed via the pointer `valp`.
  *
  * `void _MCF_atomic_store_pWIDTH_ORDER(void* mem, const void* valp);`  */
-#define __MCF_ATOMIC_PROTOTYPE_(INTEGER, SUFFIX)  \
+#define __MCF_ATOMIC_FUNCTION_(INTEGER, SUFFIX, ORDER_SUCC, ORDER_FAIL)  \
   void  \
-  _MCF_atomic_store_p##SUFFIX(void* __mem, const void* __valp) __MCF_NOEXCEPT
-
-#define __MCF_ATOMIC_STATEMENTS_(INTEGER, ORDER_SUCC, ORDER_FAIL)  \
-    INTEGER __val = *(const INTEGER*) __valp;  \
-    __atomic_store_n((INTEGER*) __mem, __val, ORDER_SUCC);
+  _MCF_atomic_store_p##SUFFIX(void* __mem, const void* __valp) __MCF_NOEXCEPT  \
+    {  \
+      INTEGER __val = *(const INTEGER*) __valp;  \
+      __atomic_store_n((INTEGER*) __mem, __val, ORDER_SUCC);  \
+    }
 
 #define __MCF_ATOMIC_HAS_RELEASE_
 #include "xatomic.i"
@@ -79,12 +79,12 @@
  * The value to write is passed by value as an integer.
  *
  * `INTEGER _MCF_atomic_xchg_WIDTH_ORDER(void* mem, INTEGER val);`  */
-#define __MCF_ATOMIC_PROTOTYPE_(INTEGER, SUFFIX)  \
+#define __MCF_ATOMIC_FUNCTION_(INTEGER, SUFFIX, ORDER_SUCC, ORDER_FAIL)  \
   INTEGER  \
-  _MCF_atomic_xchg_##SUFFIX(void* __mem, INTEGER __val) __MCF_NOEXCEPT
-
-#define __MCF_ATOMIC_STATEMENTS_(INTEGER, ORDER_SUCC, ORDER_FAIL)  \
-    return __atomic_exchange_n((INTEGER*) __mem, __val, ORDER_SUCC);
+  _MCF_atomic_xchg_##SUFFIX(void* __mem, INTEGER __val) __MCF_NOEXCEPT  \
+    {  \
+      return __atomic_exchange_n((INTEGER*) __mem, __val, ORDER_SUCC);  \
+    }
 
 #define __MCF_ATOMIC_HAS_ACQUIRE_
 #define __MCF_ATOMIC_HAS_RELEASE_
@@ -95,14 +95,14 @@
  * The value to write is passed via the pointer `valp`.
  *
  * `void _MCF_atomic_xchg_pWIDTH_ORDER(void* retp, void* mem, const void* valp);`  */
-#define __MCF_ATOMIC_PROTOTYPE_(INTEGER, SUFFIX)  \
+#define __MCF_ATOMIC_FUNCTION_(INTEGER, SUFFIX, ORDER_SUCC, ORDER_FAIL)  \
   void  \
-  _MCF_atomic_xchg_p##SUFFIX(void* __retp, void* __mem, const void* __valp) __MCF_NOEXCEPT
-
-#define __MCF_ATOMIC_STATEMENTS_(INTEGER, ORDER_SUCC, ORDER_FAIL)  \
-    INTEGER __val = *(const INTEGER*) __valp;  \
-    __val = __atomic_exchange_n((INTEGER*) __mem, __val, ORDER_SUCC);  \
-    *(INTEGER*) __retp = __val;
+  _MCF_atomic_xchg_p##SUFFIX(void* __retp, void* __mem, const void* __valp) __MCF_NOEXCEPT  \
+    {  \
+      INTEGER __val = *(const INTEGER*) __valp;  \
+      __val = __atomic_exchange_n((INTEGER*) __mem, __val, ORDER_SUCC);  \
+      *(INTEGER*) __retp = __val;  \
+    }
 
 #define __MCF_ATOMIC_HAS_ACQUIRE_
 #define __MCF_ATOMIC_HAS_RELEASE_
@@ -113,16 +113,15 @@
  * The value to write is passed by value as an integer.
  *
  * `bool _MCF_atomic_cmpxchg_WIDTH_ORDER(void* mem, void* cmpp, INTEGER val);`  */
-#define __MCF_ATOMIC_PROTOTYPE_(INTEGER, SUFFIX)  \
+#define __MCF_ATOMIC_FUNCTION_(INTEGER, SUFFIX, ORDER_SUCC, ORDER_FAIL)  \
   bool  \
-  _MCF_atomic_cmpxchg_##SUFFIX(void* __mem, void* __cmpp, INTEGER __val) __MCF_NOEXCEPT
-
-#define __MCF_ATOMIC_STATEMENTS_(INTEGER, ORDER_SUCC, ORDER_FAIL)  \
-    INTEGER __cmp = *(INTEGER*) __cmpp;  \
-    bool __r = __atomic_compare_exchange_n((INTEGER*) __mem,  \
-          &__cmp, __val, 0, ORDER_SUCC, ORDER_FAIL);  \
-    *(INTEGER*) __cmpp = __cmp;  \
-    return __r;
+  _MCF_atomic_cmpxchg_##SUFFIX(void* __mem, void* __cmpp, INTEGER __val) __MCF_NOEXCEPT  \
+    {  \
+      INTEGER __cmp = *(INTEGER*) __cmpp;  \
+      bool __r = __atomic_compare_exchange_n((INTEGER*) __mem, &__cmp, __val, 0, ORDER_SUCC, ORDER_FAIL);  \
+      *(INTEGER*) __cmpp = __cmp;  \
+      return __r;  \
+    }
 
 #define __MCF_ATOMIC_HAS_ACQUIRE_
 #define __MCF_ATOMIC_HAS_RELEASE_
@@ -133,17 +132,16 @@
  * The value to write is passed by value as an integer.
  *
  * `bool _MCF_atomic_cmpxchg_pWIDTH_ORDER(void* mem, void* cmpp, const void* val);`  */
-#define __MCF_ATOMIC_PROTOTYPE_(INTEGER, SUFFIX)  \
+#define __MCF_ATOMIC_FUNCTION_(INTEGER, SUFFIX, ORDER_SUCC, ORDER_FAIL)  \
   bool  \
-  _MCF_atomic_cmpxchg_p##SUFFIX(void* __mem, void* __cmpp, const void* __valp) __MCF_NOEXCEPT
-
-#define __MCF_ATOMIC_STATEMENTS_(INTEGER, ORDER_SUCC, ORDER_FAIL)  \
-    INTEGER __cmp = *(INTEGER*) __cmpp;  \
-    INTEGER __val = *(const INTEGER*) __valp;  \
-    bool __r = __atomic_compare_exchange_n((INTEGER*) __mem,  \
-          &__cmp, __val, 0, ORDER_SUCC, ORDER_FAIL);  \
-    *(INTEGER*) __cmpp = __cmp;  \
-    return __r;
+  _MCF_atomic_cmpxchg_p##SUFFIX(void* __mem, void* __cmpp, const void* __valp) __MCF_NOEXCEPT  \
+    {  \
+      INTEGER __cmp = *(INTEGER*) __cmpp;  \
+      INTEGER __val = *(const INTEGER*) __valp;  \
+      bool __r = __atomic_compare_exchange_n((INTEGER*) __mem, &__cmp, __val, 0, ORDER_SUCC, ORDER_FAIL);  \
+      *(INTEGER*) __cmpp = __cmp;  \
+      return __r;  \
+    }
 
 #define __MCF_ATOMIC_HAS_ACQUIRE_
 #define __MCF_ATOMIC_HAS_RELEASE_
@@ -154,16 +152,15 @@
  * The value to write is passed by value as an integer.
  *
  * `bool _MCF_atomic_cmpxchg_weak_WIDTH_ORDER(void* mem, void* cmpp, INTEGER val);`  */
-#define __MCF_ATOMIC_PROTOTYPE_(INTEGER, SUFFIX)  \
+#define __MCF_ATOMIC_FUNCTION_(INTEGER, SUFFIX, ORDER_SUCC, ORDER_FAIL)  \
   bool  \
-  _MCF_atomic_cmpxchg_weak_##SUFFIX(void* __mem, void* __cmpp, INTEGER __val) __MCF_NOEXCEPT
-
-#define __MCF_ATOMIC_STATEMENTS_(INTEGER, ORDER_SUCC, ORDER_FAIL)  \
-    INTEGER __cmp = *(INTEGER*) __cmpp;  \
-    bool __r = __atomic_compare_exchange_n((INTEGER*) __mem,  \
-          &__cmp, __val, 1, ORDER_SUCC, ORDER_FAIL);  \
-    *(INTEGER*) __cmpp = __cmp;  \
-    return __r;
+  _MCF_atomic_cmpxchg_weak_##SUFFIX(void* __mem, void* __cmpp, INTEGER __val) __MCF_NOEXCEPT  \
+    {  \
+      INTEGER __cmp = *(INTEGER*) __cmpp;  \
+      bool __r = __atomic_compare_exchange_n((INTEGER*) __mem, &__cmp, __val, 1, ORDER_SUCC, ORDER_FAIL);  \
+      *(INTEGER*) __cmpp = __cmp;  \
+      return __r;  \
+    }
 
 #define __MCF_ATOMIC_HAS_ACQUIRE_
 #define __MCF_ATOMIC_HAS_RELEASE_
@@ -174,17 +171,16 @@
  * The value to write is passed by value as an integer.
  *
  * `bool _MCF_atomic_cmpxchg_weak_pWIDTH_ORDER(void* mem, void* cmpp, const void* val);`  */
-#define __MCF_ATOMIC_PROTOTYPE_(INTEGER, SUFFIX)  \
+#define __MCF_ATOMIC_FUNCTION_(INTEGER, SUFFIX, ORDER_SUCC, ORDER_FAIL)  \
   bool  \
-  _MCF_atomic_cmpxchg_weak_p##SUFFIX(void* __mem, void* __cmpp, const void* __valp) __MCF_NOEXCEPT
-
-#define __MCF_ATOMIC_STATEMENTS_(INTEGER, ORDER_SUCC, ORDER_FAIL)  \
-    INTEGER __cmp = *(INTEGER*) __cmpp;  \
-    INTEGER __val = *(const INTEGER*) __valp;  \
-    bool __r = __atomic_compare_exchange_n((INTEGER*) __mem,  \
-          &__cmp, __val, 1, ORDER_SUCC, ORDER_FAIL);  \
-    *(INTEGER*) __cmpp = __cmp;  \
-    return __r;  \
+  _MCF_atomic_cmpxchg_weak_p##SUFFIX(void* __mem, void* __cmpp, const void* __valp) __MCF_NOEXCEPT  \
+    {  \
+      INTEGER __cmp = *(INTEGER*) __cmpp;  \
+      INTEGER __val = *(const INTEGER*) __valp;  \
+      bool __r = __atomic_compare_exchange_n((INTEGER*) __mem, &__cmp, __val, 1, ORDER_SUCC, ORDER_FAIL);  \
+      *(INTEGER*) __cmpp = __cmp;  \
+      return __r;  \
+    }
 
 #define __MCF_ATOMIC_HAS_ACQUIRE_
 #define __MCF_ATOMIC_HAS_RELEASE_
@@ -195,12 +191,12 @@
  * The value to add is passed by value as an integer.
  *
  * `INTEGER val __MCF_atomic_xadd_WIDTH_ORDER(void* mem, INTEGER val);`  */
-#define __MCF_ATOMIC_PROTOTYPE_(INTEGER, SUFFIX)  \
+#define __MCF_ATOMIC_FUNCTION_(INTEGER, SUFFIX, ORDER_SUCC, ORDER_FAIL)  \
   INTEGER  \
-  _MCF_atomic_xadd_##SUFFIX(void* __mem, INTEGER __val) __MCF_NOEXCEPT
-
-#define __MCF_ATOMIC_STATEMENTS_(INTEGER, ORDER_SUCC, ORDER_FAIL)  \
-    return __atomic_fetch_add((INTEGER*) __mem, __val, ORDER_SUCC);  \
+  _MCF_atomic_xadd_##SUFFIX(void* __mem, INTEGER __val) __MCF_NOEXCEPT  \
+    {  \
+      return __atomic_fetch_add((INTEGER*) __mem, __val, ORDER_SUCC);  \
+    }
 
 #define __MCF_ATOMIC_HAS_ACQUIRE_
 #define __MCF_ATOMIC_HAS_RELEASE_
@@ -211,12 +207,12 @@
  * The value to subtract is passed by value as an integer.
  *
  * `INTEGER val __MCF_atomic_xsub_WIDTH_ORDER(void* mem, INTEGER val);`  */
-#define __MCF_ATOMIC_PROTOTYPE_(INTEGER, SUFFIX)  \
+#define __MCF_ATOMIC_FUNCTION_(INTEGER, SUFFIX, ORDER_SUCC, ORDER_FAIL)  \
   INTEGER  \
-  _MCF_atomic_xsub_##SUFFIX(void* __mem, INTEGER __val) __MCF_NOEXCEPT
-
-#define __MCF_ATOMIC_STATEMENTS_(INTEGER, ORDER_SUCC, ORDER_FAIL)  \
-    return __atomic_fetch_sub((INTEGER*) __mem, __val, ORDER_SUCC);  \
+  _MCF_atomic_xsub_##SUFFIX(void* __mem, INTEGER __val) __MCF_NOEXCEPT  \
+    {  \
+      return __atomic_fetch_sub((INTEGER*) __mem, __val, ORDER_SUCC);  \
+    }
 
 #define __MCF_ATOMIC_HAS_ACQUIRE_
 #define __MCF_ATOMIC_HAS_RELEASE_
