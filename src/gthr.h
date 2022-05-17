ab30d5bcfe85118f8451e5531a66c898fc645454
LIU Hao <lh_mouse@126.com>
2022-05-17 21:49:23 +0800

    gthr: Add `constexpr` specifier to `__MCF_gthr_equal()`

--- src/gthr.h
+++ src/gthr.h
@@ -657,6 +657,7 @@ __MCF_gthr_self(void) __MCF_NOEXCEPT
 
 /* Checks whether two thread IDs compare equal, like `pthread_equal()`.  */
 __MCF_DECLSPEC_GTHR(__MCF_GNU_INLINE)
+__MCF_CXX11(constexpr)
 int
 __MCF_gthr_equal(__gthread_t __t1, __gthread_t __t2) __MCF_NOEXCEPT
   __attribute__((__pure__));
@@ -664,6 +665,7 @@ __MCF_gthr_equal(__gthread_t __t1, __gthread_t __t2) __MCF_NOEXCEPT
 #define __gthread_equal  __MCF_thrd_equal
 
 __MCF_DECLSPEC_GTHR(__MCF_GNU_INLINE)
+__MCF_CXX11(constexpr)
 int
 __MCF_gthr_equal(__gthread_t __t1, __gthread_t __t2) __MCF_NOEXCEPT
   {
