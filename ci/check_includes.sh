d01cc215b00a91f6e71de35a48552c329659c28f
LIU Hao <lh_mouse@126.com>
2022-05-23 22:36:26 +0800

    ci/check_includes: Add checks for C++14

--- ci/check_includes.sh
+++ ci/check_includes.sh
@@ -23,6 +23,12 @@ do
   ${CC} ${CPPFLAGS} ${CFLAGS} -x c++ -std=c++11 -Wpedantic -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
 done
 
+for _file in $(find -L "src" -name "*.h")
+do
+  echo "Checking header as C++14:  ${_cmd}  \"${_file}\""
+  ${CC} ${CPPFLAGS} ${CFLAGS} -x c++ -std=c++14 -Wpedantic -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
+done
+
 for _file in $(find -L "src" -name "*.c")
 do
   echo "Checking source as C99:  ${_cmd}  \"${_file}\""
