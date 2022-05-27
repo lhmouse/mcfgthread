404f513b26c4999857b89ae16bf5e4583b09e19e
LIU Hao <lh_mouse@126.com>
2022-05-27 21:51:28 +0800

    test/gthr_inline_alias: Update

--- test/gthr_inline_alias.c
+++ test/gthr_inline_alias.c
@@ -4,20 +4,67 @@
 
 #include "../src/gthr.h"
 #include <assert.h>
-#include <stdio.h>
 
 int
 main(void)
   {
     volatile intptr_t mcfp, gthp;
 
+    mcfp = (intptr_t) __MCF_gthr_active_p;
+    gthp = (intptr_t) __gthread_active_p;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_gthr_getspecific;
+    gthp = (intptr_t) __gthread_getspecific;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_gthr_key_create;
+    gthp = (intptr_t) __gthread_key_create;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_gthr_key_delete;
+    gthp = (intptr_t) __gthread_key_delete;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_gthr_mutex_destroy;
+    gthp = (intptr_t) __gthread_mutex_destroy;
+    assert(mcfp == gthp);
+
     mcfp = (intptr_t) __MCF_gthr_mutex_lock;
-    printf("mcfp = %tx\n", mcfp);
-
     gthp = (intptr_t) __gthread_mutex_lock;
-    printf("gthp = %tx\n", gthp);
+    assert(mcfp == gthp);
 
-    assert(mcfp = gthp);
+    mcfp = (intptr_t) __MCF_gthr_mutex_trylock;
+    gthp = (intptr_t) __gthread_mutex_trylock;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_gthr_mutex_unlock;
+    gthp = (intptr_t) __gthread_mutex_unlock;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_gthr_once;
+    gthp = (intptr_t) __gthread_once;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_gthr_recursive_mutex_destroy;
+    gthp = (intptr_t) __gthread_recursive_mutex_destroy;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_gthr_recursive_mutex_lock;
+    gthp = (intptr_t) __gthread_recursive_mutex_lock;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_gthr_recursive_mutex_trylock;
+    gthp = (intptr_t) __gthread_recursive_mutex_trylock;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_gthr_recursive_mutex_unlock;
+    gthp = (intptr_t) __gthread_recursive_mutex_unlock;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_gthr_setspecific;
+    gthp = (intptr_t) __gthread_setspecific;
+    assert(mcfp == gthp);
 
     return 0;
   }
