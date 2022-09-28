c7f76b32e380e43cc6f200d62aa525cbd68bd376
LIU Hao <lh_mouse@126.com>
2022-09-28 23:02:33 +0800

    *: Reformat calling convention specifiers
    This is done by
    
      sed -Ei 's/ (__[a-z]+(decl|call))$/\n\1/g' src/*.[hci]

--- src/memcpy.c
+++ src/memcpy.c
@@ -4,15 +4,18 @@
 
 #include "precompiled.i"
 
-void* __cdecl
+void*
+__cdecl
 __MCF_mcopy(void* dst, const void* src, size_t size);
 
 __MCF_DLLEXPORT
-void* __cdecl
+void*
+__cdecl
 memcpy(void* dst, const void* src, size_t size);
 
 __MCF_DLLEXPORT
-void* __cdecl
+void*
+__cdecl
 memcpy(void* dst, const void* src, size_t size)
   {
     return __MCF_mcopy(dst, src, size);
