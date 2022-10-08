cdbffb360093561c12df116bac3a5c59c82a5792
LIU Hao <lh_mouse@126.com>
2022-10-08 15:46:00 +0800

    mutex: Add a branch hint for the spin count
    (cherry picked from commit 9ea79009c60142a59a0be3f4ebab2611a9ba0a7a)
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- src/mutex.c
+++ src/mutex.c
@@ -81,7 +81,8 @@ _MCF_mutex_lock_slow(_MCF_mutex* mutex, const int64_t* timeout_opt)
       __MCF_ASSERT(spin > 0);
       spin *= (int) (__MCF_MUTEX_MAX_SPIN_COUNT / __MCF_MUTEX_SP_NFAIL_THRESHOLD);
 
-      while(--spin >= 0) {
+      while(__builtin_expect(spin, 100) > 0) {
+        spin --;
         YieldProcessor();
 
         /* Wait for my turn.  */
