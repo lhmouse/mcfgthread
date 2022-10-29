625c1c31651965938ae1399e6f4f2f0f94a7b2e6
LIU Hao <lh_mouse@126.com>
2022-10-30 02:36:44 +0800

    fwd: Update `__MCF_STATIC_ASSERT()`
    (cherry picked from commit c12db271971209f81691d33719101d74d0a17fba)
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- test/gthr_inline_alias.c
+++ test/gthr_inline_alias.c
@@ -8,63 +8,20 @@
 int
 main(void)
   {
-    volatile intptr_t mcfp, gthp;
-
-    mcfp = (intptr_t) __MCF_gthr_active_p;
-    gthp = (intptr_t) __gthread_active_p;
-    assert(mcfp == gthp);
-
-    mcfp = (intptr_t) __MCF_gthr_getspecific;
-    gthp = (intptr_t) __gthread_getspecific;
-    assert(mcfp == gthp);
-
-    mcfp = (intptr_t) __MCF_gthr_key_create;
-    gthp = (intptr_t) __gthread_key_create;
-    assert(mcfp == gthp);
-
-    mcfp = (intptr_t) __MCF_gthr_key_delete;
-    gthp = (intptr_t) __gthread_key_delete;
-    assert(mcfp == gthp);
-
-    mcfp = (intptr_t) __MCF_gthr_mutex_destroy;
-    gthp = (intptr_t) __gthread_mutex_destroy;
-    assert(mcfp == gthp);
-
-    mcfp = (intptr_t) __MCF_gthr_mutex_lock;
-    gthp = (intptr_t) __gthread_mutex_lock;
-    assert(mcfp == gthp);
-
-    mcfp = (intptr_t) __MCF_gthr_mutex_trylock;
-    gthp = (intptr_t) __gthread_mutex_trylock;
-    assert(mcfp == gthp);
-
-    mcfp = (intptr_t) __MCF_gthr_mutex_unlock;
-    gthp = (intptr_t) __gthread_mutex_unlock;
-    assert(mcfp == gthp);
-
-    mcfp = (intptr_t) __MCF_gthr_once;
-    gthp = (intptr_t) __gthread_once;
-    assert(mcfp == gthp);
-
-    mcfp = (intptr_t) __MCF_gthr_recursive_mutex_destroy;
-    gthp = (intptr_t) __gthread_recursive_mutex_destroy;
-    assert(mcfp == gthp);
-
-    mcfp = (intptr_t) __MCF_gthr_recursive_mutex_lock;
-    gthp = (intptr_t) __gthread_recursive_mutex_lock;
-    assert(mcfp == gthp);
-
-    mcfp = (intptr_t) __MCF_gthr_recursive_mutex_trylock;
-    gthp = (intptr_t) __gthread_recursive_mutex_trylock;
-    assert(mcfp == gthp);
-
-    mcfp = (intptr_t) __MCF_gthr_recursive_mutex_unlock;
-    gthp = (intptr_t) __gthread_recursive_mutex_unlock;
-    assert(mcfp == gthp);
-
-    mcfp = (intptr_t) __MCF_gthr_setspecific;
-    gthp = (intptr_t) __gthread_setspecific;
-    assert(mcfp == gthp);
+    __MCF_STATIC_ASSERT(__MCF_gthr_active_p == __gthread_active_p);
+    __MCF_STATIC_ASSERT(__MCF_gthr_getspecific == __gthread_getspecific);
+    __MCF_STATIC_ASSERT(__MCF_gthr_key_create == __gthread_key_create);
+    __MCF_STATIC_ASSERT(__MCF_gthr_key_delete == __gthread_key_delete);
+    __MCF_STATIC_ASSERT(__MCF_gthr_mutex_destroy == __gthread_mutex_destroy);
+    __MCF_STATIC_ASSERT(__MCF_gthr_mutex_lock == __gthread_mutex_lock);
+    __MCF_STATIC_ASSERT(__MCF_gthr_mutex_trylock == __gthread_mutex_trylock);
+    __MCF_STATIC_ASSERT(__MCF_gthr_mutex_unlock == __gthread_mutex_unlock);
+    __MCF_STATIC_ASSERT(__MCF_gthr_once == __gthread_once);
+    __MCF_STATIC_ASSERT(__MCF_gthr_recursive_mutex_destroy == __gthread_recursive_mutex_destroy);
+    __MCF_STATIC_ASSERT(__MCF_gthr_recursive_mutex_lock == __gthread_recursive_mutex_lock);
+    __MCF_STATIC_ASSERT(__MCF_gthr_recursive_mutex_trylock == __gthread_recursive_mutex_trylock);
+    __MCF_STATIC_ASSERT(__MCF_gthr_recursive_mutex_unlock == __gthread_recursive_mutex_unlock);
+    __MCF_STATIC_ASSERT(__MCF_gthr_setspecific == __gthread_setspecific);
 
     return 0;
   }
