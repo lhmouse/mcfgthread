005747197ce65e3aa8817a727671c20bc91d44be
LIU Hao <lh_mouse@126.com>
2022-10-30 02:21:52 +0800

    gthr,gthr_libobjc,c11,libcxx: Allow aliases to be disabled
    (cherry picked from commit 643ca26bf405d3ddd16ed43167dc299b8bbe39de)
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- src/c11.h
+++ src/c11.h
@@ -81,175 +81,225 @@ __MCF_DECLSPEC_C11_INLINE
 void
 __MCF_c11_call_once(once_flag* __flag, __MCF_once_callback* __init_func);
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(call_once, __MCF_c11_call_once);
+#endif
 
 /* 7.26.3.1 The cnd_broadcast function  */
 __MCF_DECLSPEC_C11_INLINE
 int
 __MCF_c11_cnd_broadcast(cnd_t* __cond) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(cnd_broadcast, __MCF_c11_cnd_broadcast);
+#endif
 
 /* 7.26.3.2 The cnd_destroy function  */
 __MCF_DECLSPEC_C11_INLINE
 void
 __MCF_c11_cnd_destroy(cnd_t* __cond) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(cnd_destroy, __MCF_c11_cnd_destroy);
+#endif
 
 /* 7.26.3.3 The cnd_init function  */
 __MCF_DECLSPEC_C11_INLINE
 int
 __MCF_c11_cnd_init(cnd_t* __cond) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(cnd_init, __MCF_c11_cnd_init);
+#endif
 
 /* 7.26.3.4 The cnd_signal function  */
 __MCF_DECLSPEC_C11_INLINE
 int
 __MCF_c11_cnd_signal(cnd_t* __cond) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(cnd_signal, __MCF_c11_cnd_signal);
+#endif
 
 /* 7.26.3.5 The cnd_timedwait function  */
 __MCF_DECLSPEC_C11_INLINE
 int
 __MCF_c11_cnd_timedwait(cnd_t* __cond, mtx_t* __mtx, const struct timespec* __ts) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(cnd_timedwait, __MCF_c11_cnd_timedwait);
+#endif
 
 /* 7.26.3.6 The cnd_wait function  */
 __MCF_DECLSPEC_C11_INLINE
 int
 __MCF_c11_cnd_wait(cnd_t* __cond, mtx_t* __mtx) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(cnd_wait, __MCF_c11_cnd_wait);
+#endif
 
 /* 7.26.4.1 The mtx_destroy function  */
 __MCF_DECLSPEC_C11_INLINE
 void
 __MCF_c11_mtx_destroy(mtx_t* __mtx) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(mtx_destroy, __MCF_c11_mtx_destroy);
+#endif
 
 /* 7.26.4.2 The mtx_init function  */
 __MCF_DECLSPEC_C11_INLINE
 int
 __MCF_c11_mtx_init(mtx_t* __mtx, int __type) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(mtx_init, __MCF_c11_mtx_init);
+#endif
 
 /* 7.26.4.3 The mtx_lock function  */
 __MCF_DECLSPEC_C11_INLINE
 int
 __MCF_c11_mtx_lock(mtx_t* __mtx) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(mtx_lock, __MCF_c11_mtx_lock);
+#endif
 
 /* 7.26.4.4 The mtx_timedlock function  */
 __MCF_DECLSPEC_C11_INLINE
 int
 __MCF_c11_mtx_timedlock(mtx_t* __mtx, const struct timespec* __ts) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(mtx_timedlock, __MCF_c11_mtx_timedlock);
+#endif
 
 /* 7.26.4.5 The mtx_trylock function  */
 __MCF_DECLSPEC_C11_INLINE
 int
 __MCF_c11_mtx_trylock(mtx_t* __mtx) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(mtx_trylock, __MCF_c11_mtx_trylock);
+#endif
 
 /* 7.26.4.6 The mtx_unlock function  */
 __MCF_DECLSPEC_C11_INLINE
 int
 __MCF_c11_mtx_unlock(mtx_t* __mtx) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(mtx_unlock, __MCF_c11_mtx_unlock);
+#endif
 
 /* 7.26.5.1 The thrd_create function  */
 __MCF_DECLSPEC_C11_INLINE
 int
 __MCF_c11_thrd_create(thrd_t* __thrd, thrd_start_t __proc, void* __arg) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(thrd_create, __MCF_c11_thrd_create);
+#endif
 
 /* 7.26.5.2 The thrd_current function  */
 __MCF_DECLSPEC_C11_INLINE
 thrd_t
 __MCF_c11_thrd_current(void) __MCF_NOEXCEPT __attribute__((__const__, __returns_nonnull__));
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(thrd_current, __MCF_c11_thrd_current);
+#endif
 
 /* 7.26.5.3 The thrd_detach function  */
 __MCF_DECLSPEC_C11_INLINE
 int
 __MCF_c11_thrd_detach(thrd_t __thrd) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(thrd_detach, __MCF_c11_thrd_detach);
+#endif
 
 /* 7.26.5.4 The thrd_equal function  */
 __MCF_DECLSPEC_C11_INLINE __MCF_CXX11(constexpr)
 int
 __MCF_c11_thrd_equal(thrd_t __t1, thrd_t __t2) __MCF_NOEXCEPT __attribute__((__pure__));
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(thrd_equal, __MCF_c11_thrd_equal);
+#endif
 
 /* 7.26.5.5 The thrd_exit function  */
 __MCF_DECLSPEC_C11_INLINE
 void
 __MCF_c11_thrd_exit(int __res) __MCF_NOEXCEPT __attribute__((__noreturn__));
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(thrd_exit, __MCF_c11_thrd_exit);
+#endif
 
 /* 7.26.5.6 The thrd_join function  */
 __MCF_DECLSPEC_C11_INLINE
 int
 __MCF_c11_thrd_join(thrd_t __thrd, int* __resp_opt) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(thrd_join, __MCF_c11_thrd_join);
+#endif
 
 /* 7.26.5.7 The thrd_sleep function  */
 __MCF_DECLSPEC_C11_IMPORT
 int
 __MCF_c11_thrd_sleep(const struct timespec* __dur, struct timespec* __rem_opt) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(thrd_sleep, __MCF_c11_thrd_sleep);
+#endif
 
 /* 7.26.5.8 The thrd_yield function  */
 __MCF_DECLSPEC_C11_INLINE
 void
 __MCF_c11_thrd_yield(void) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(thrd_yield, __MCF_c11_thrd_yield);
+#endif
 
 /* 7.26.6.1 The tss_create function  */
 __MCF_DECLSPEC_C11_INLINE
 int
 __MCF_c11_tss_create(tss_t* __keyp, tss_dtor_t __dtor_opt) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(tss_create, __MCF_c11_tss_create);
+#endif
 
 /* 7.26.6.2 The tss_delete function  */
 __MCF_DECLSPEC_C11_INLINE
 void
 __MCF_c11_tss_delete(tss_t __key) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(tss_delete, __MCF_c11_tss_delete);
+#endif
 
 /* 7.26.6.3 The tss_get function  */
 __MCF_DECLSPEC_C11_INLINE
 void*
 __MCF_c11_tss_get(tss_t __key) __MCF_NOEXCEPT __attribute__((__pure__));
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(tss_get, __MCF_c11_tss_get);
+#endif
 
 /* 7.26.6.4 The tss_set function  */
 __MCF_DECLSPEC_C11_INLINE
 int
 __MCF_c11_tss_set(tss_t __key, void* __val_opt) __MCF_NOEXCEPT;
 
+#ifndef __MCF_C11_NO_ALIASES
 __MCF_ALIAS(tss_set, __MCF_c11_tss_set);
+#endif
 
 /* Define inline functions after all declarations.
  * We would like to keep them away from declarations for conciseness, which also
