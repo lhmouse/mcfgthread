7191798616bd31663de4d2f4ee192a991a486fc6
LIU Hao <lh_mouse@126.com>
2022-10-30 22:24:24 +0800

    ci: Add checks for static libraries
    (cherry picked from commit ff532d60df9b312154d77dfd632a571330ee2740)
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- ci/build.sh
+++ ci/build.sh
@@ -4,23 +4,6 @@
 export CC=${CC:-"gcc"}
 export CCFLAGS='-O2 -g0'
 
-_fail=1
-
-while test $# -gt 0
-do
-  case $1 in
-    --disable-make-check )
-      _fail=0
-      shift 1
-      ;;
-
-    * )
-      echo "WARNING: unknown option -- '$1'" >&2
-      shift 1
-      ;;
-  esac
-done
-
 # build
 ${CC} --version
 mkdir -p m4
@@ -28,11 +11,11 @@ autoreconf -ifv
 
 cd $(mktemp -d)
 trap 'rm -rf ~+ || true' EXIT
-~-/configure --disable-silent-rules --enable-debug-checks --disable-static
+~-/configure --disable-silent-rules --enable-debug-checks ${CONFIGURE_OPTIONS}
 
 # test
 if ! make -j$(nproc) distcheck
 then
   cat ./test-suite.log
-  exit ${_fail}
+  exit 1
 fi
