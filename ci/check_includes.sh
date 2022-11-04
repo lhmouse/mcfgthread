bbe334d05489c3c0ae4020daaeccdd6c2d01766c
LIU Hao <lh_mouse@126.com>
2022-11-04 16:45:20 +0800

    libcxx,ci: Fix 'check_includes.sh'
    (cherry picked from commit 4dcc6def5aa53046d3282cd686c05089a56c48f5)
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- ci/check_includes.sh
+++ ci/check_includes.sh
@@ -3,7 +3,7 @@
 # setup
 export CC=${CC:-"gcc"}
 export CXX=${CXX:-"g++"}
-export CPPFLAGS="-D_WIN32_WINNT=0x0601 -Wall -Wextra"
+export CPPFLAGS="-D_WIN32_WINNT=0x0601 -Wall -Wextra "${CPPFLAGS}
 
 for _file in $(find -L "src" -name "*.h")
 do
@@ -20,17 +20,17 @@ done
 for _file in $(find -L "src" -name "*.h")
 do
   echo "Checking header as C++11:  ${_cmd}  \"${_file}\""
-  ${CC} ${CPPFLAGS} ${CFLAGS} -x c++ -std=c++11 -Wpedantic -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
+  ${CC} ${CPPFLAGS} ${CFLAGS} -x c++ -std=c++11 -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
 done
 
 for _file in $(find -L "src" -name "*.h")
 do
   echo "Checking header as C++14:  ${_cmd}  \"${_file}\""
-  ${CC} ${CPPFLAGS} ${CFLAGS} -x c++ -std=c++14 -Wpedantic -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
+  ${CC} ${CPPFLAGS} ${CFLAGS} -x c++ -std=c++14 -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
 done
 
 for _file in $(find -L "src" -name "*.c")
 do
   echo "Checking source as C99:  ${_cmd}  \"${_file}\""
-  ${CC} ${CPPFLAGS} ${CFLAGS} -x c -std=c99 -Wpedantic -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
+  ${CC} ${CPPFLAGS} ${CFLAGS} -x c -std=c99 -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
 done
