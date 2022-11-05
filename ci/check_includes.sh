e3be4313c90fadd784759899c7603d2642a9826b
LIU Hao <lh_mouse@126.com>
2022-11-05 18:00:19 +0800

    ci: Update 'check_includes.sh'
    Do tell headers from source files, and remove the unused `CXX` variable.
    
    (cherry picked from commit 5e61052ba94c0e89e3cd4e9094c1220e7f25f053)
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- ci/check_includes.sh
+++ ci/check_includes.sh
@@ -2,31 +2,30 @@
 
 # setup
 export CC=${CC:-"gcc"}
-export CXX=${CXX:-"g++"}
 export CPPFLAGS="-D_WIN32_WINNT=0x0601 -Wall -Wextra "${CPPFLAGS}
 
 for _file in $(find -L "src" -name "*.h")
 do
   echo "Checking header as C89:  ${_cmd}  \"${_file}\""
-  ${CC} ${CPPFLAGS} ${CFLAGS} -x c -std=c89 -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
+  ${CC} ${CPPFLAGS} ${CFLAGS} -x c-header -std=c89 -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
 done
 
 for _file in $(find -L "src" -name "*.h")
 do
   echo "Checking header as C++98:  ${_cmd}  \"${_file}\""
-  ${CC} ${CPPFLAGS} ${CFLAGS} -x c++ -std=c++98 -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
+  ${CC} ${CPPFLAGS} ${CFLAGS} -x c++-header -std=c++98 -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
 done
 
 for _file in $(find -L "src" -name "*.h")
 do
   echo "Checking header as C++11:  ${_cmd}  \"${_file}\""
-  ${CC} ${CPPFLAGS} ${CFLAGS} -x c++ -std=c++11 -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
+  ${CC} ${CPPFLAGS} ${CFLAGS} -x c++-header -std=c++11 -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
 done
 
 for _file in $(find -L "src" -name "*.h")
 do
   echo "Checking header as C++14:  ${_cmd}  \"${_file}\""
-  ${CC} ${CPPFLAGS} ${CFLAGS} -x c++ -std=c++14 -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
+  ${CC} ${CPPFLAGS} ${CFLAGS} -x c++-header -std=c++14 -fsyntax-only -DHAVE_CONFIG_H -I. ${_file}
 done
 
 for _file in $(find -L "src" -name "*.c")
