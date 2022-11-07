0cce0a6f154ad773842296e8a05bb73bbab1cc17
LIU Hao <lh_mouse@126.com>
2022-11-07 13:21:21 +0800

    test/{gthr,c11,libcxx}_inline_alias: Revert to run-time checks

--- test/c11_inline_alias.c
+++ test/c11_inline_alias.c
@@ -8,31 +8,107 @@
 int
 main(void)
   {
-    __MCF_STATIC_ASSERT(__MCF_c11_call_once == call_once);
-    __MCF_STATIC_ASSERT(__MCF_c11_cnd_broadcast == cnd_broadcast);
-    __MCF_STATIC_ASSERT(__MCF_c11_cnd_destroy == cnd_destroy);
-    __MCF_STATIC_ASSERT(__MCF_c11_cnd_init == cnd_init);
-    __MCF_STATIC_ASSERT(__MCF_c11_cnd_signal == cnd_signal);
-    __MCF_STATIC_ASSERT(__MCF_c11_cnd_timedwait == cnd_timedwait);
-    __MCF_STATIC_ASSERT(__MCF_c11_cnd_wait == cnd_wait);
-    __MCF_STATIC_ASSERT(__MCF_c11_mtx_destroy == mtx_destroy);
-    __MCF_STATIC_ASSERT(__MCF_c11_mtx_init == mtx_init);
-    __MCF_STATIC_ASSERT(__MCF_c11_mtx_lock == mtx_lock);
-    __MCF_STATIC_ASSERT(__MCF_c11_mtx_timedlock == mtx_timedlock);
-    __MCF_STATIC_ASSERT(__MCF_c11_mtx_trylock == mtx_trylock);
-    __MCF_STATIC_ASSERT(__MCF_c11_mtx_unlock == mtx_unlock);
-    __MCF_STATIC_ASSERT(__MCF_c11_thrd_create == thrd_create);
-    __MCF_STATIC_ASSERT(__MCF_c11_thrd_current == thrd_current);
-    __MCF_STATIC_ASSERT(__MCF_c11_thrd_detach == thrd_detach);
-    __MCF_STATIC_ASSERT(__MCF_c11_thrd_equal == thrd_equal);
-    __MCF_STATIC_ASSERT(__MCF_c11_thrd_exit == thrd_exit);
-    __MCF_STATIC_ASSERT(__MCF_c11_thrd_join == thrd_join);
-    __MCF_STATIC_ASSERT(__MCF_c11_thrd_sleep == thrd_sleep);
-    __MCF_STATIC_ASSERT(__MCF_c11_thrd_yield == thrd_yield);
-    __MCF_STATIC_ASSERT(__MCF_c11_tss_create == tss_create);
-    __MCF_STATIC_ASSERT(__MCF_c11_tss_delete == tss_delete);
-    __MCF_STATIC_ASSERT(__MCF_c11_tss_get == tss_get);
-    __MCF_STATIC_ASSERT(__MCF_c11_tss_set == tss_set);
+    volatile intptr_t mcfp, gthp;
+
+    mcfp = (intptr_t) __MCF_c11_call_once;
+    gthp = (intptr_t) call_once;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_cnd_broadcast;
+    gthp = (intptr_t) cnd_broadcast;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_cnd_destroy;
+    gthp = (intptr_t) cnd_destroy;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_cnd_init;
+    gthp = (intptr_t) cnd_init;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_cnd_signal;
+    gthp = (intptr_t) cnd_signal;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_cnd_timedwait;
+    gthp = (intptr_t) cnd_timedwait;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_cnd_wait;
+    gthp = (intptr_t) cnd_wait;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_mtx_destroy;
+    gthp = (intptr_t) mtx_destroy;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_mtx_init;
+    gthp = (intptr_t) mtx_init;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_mtx_lock;
+    gthp = (intptr_t) mtx_lock;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_mtx_timedlock;
+    gthp = (intptr_t) mtx_timedlock;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_mtx_trylock;
+    gthp = (intptr_t) mtx_trylock;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_mtx_unlock;
+    gthp = (intptr_t) mtx_unlock;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_thrd_create;
+    gthp = (intptr_t) thrd_create;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_thrd_current;
+    gthp = (intptr_t) thrd_current;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_thrd_detach;
+    gthp = (intptr_t) thrd_detach;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_thrd_equal;
+    gthp = (intptr_t) thrd_equal;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_thrd_exit;
+    gthp = (intptr_t) thrd_exit;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_thrd_join;
+    gthp = (intptr_t) thrd_join;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_thrd_sleep;
+    gthp = (intptr_t) thrd_sleep;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_thrd_yield;
+    gthp = (intptr_t) thrd_yield;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_tss_create;
+    gthp = (intptr_t) tss_create;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_tss_delete;
+    gthp = (intptr_t) tss_delete;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_tss_get;
+    gthp = (intptr_t) tss_get;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_c11_tss_set;
+    gthp = (intptr_t) tss_set;
+    assert(mcfp == gthp);
 
     return 0;
   }
--- test/libcxx_inline_alias.c
+++ test/libcxx_inline_alias.c
@@ -8,36 +8,127 @@
 int
 main(void)
   {
-    __MCF_STATIC_ASSERT(__MCF_libcxx_condvar_broadcast == __libcpp_condvar_broadcast);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_condvar_destroy == __libcpp_condvar_destroy);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_condvar_init == __libcpp_condvar_init);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_condvar_signal == __libcpp_condvar_signal);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_condvar_timedwait == __libcpp_condvar_timedwait);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_condvar_wait == __libcpp_condvar_wait);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_execute_once == __libcpp_execute_once);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_mutex_destroy == __libcpp_mutex_destroy);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_mutex_init == __libcpp_mutex_init);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_mutex_lock == __libcpp_mutex_lock);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_mutex_trylock == __libcpp_mutex_trylock);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_mutex_unlock == __libcpp_mutex_unlock);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_recursive_mutex_destroy == __libcpp_recursive_mutex_destroy);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_recursive_mutex_init == __libcpp_recursive_mutex_init);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_recursive_mutex_lock == __libcpp_recursive_mutex_lock);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_recursive_mutex_trylock == __libcpp_recursive_mutex_trylock);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_recursive_mutex_unlock == __libcpp_recursive_mutex_unlock);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_thread_create == __libcpp_thread_create);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_thread_detach == __libcpp_thread_detach);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_thread_get_current_id == __libcpp_thread_get_current_id);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_thread_get_id == __libcpp_thread_get_id);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_thread_id_equal == __libcpp_thread_id_equal);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_thread_id_less == __libcpp_thread_id_less);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_thread_isnull == __libcpp_thread_isnull);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_thread_join == __libcpp_thread_join);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_thread_yield == __libcpp_thread_yield);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_tls_create == __libcpp_tls_create);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_tls_delete == __libcpp_tls_delete);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_tls_get == __libcpp_tls_get);
-    __MCF_STATIC_ASSERT(__MCF_libcxx_tls_set == __libcpp_tls_set);
+    volatile intptr_t mcfp, gthp;
+
+    mcfp = (intptr_t) __MCF_libcxx_condvar_broadcast;
+    gthp = (intptr_t) __libcpp_condvar_broadcast;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_condvar_destroy;
+    gthp = (intptr_t) __libcpp_condvar_destroy;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_condvar_init;
+    gthp = (intptr_t) __libcpp_condvar_init;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_condvar_signal;
+    gthp = (intptr_t) __libcpp_condvar_signal;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_condvar_timedwait;
+    gthp = (intptr_t) __libcpp_condvar_timedwait;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_condvar_wait;
+    gthp = (intptr_t) __libcpp_condvar_wait;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_execute_once;
+    gthp = (intptr_t) __libcpp_execute_once;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_mutex_destroy;
+    gthp = (intptr_t) __libcpp_mutex_destroy;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_mutex_init;
+    gthp = (intptr_t) __libcpp_mutex_init;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_mutex_lock;
+    gthp = (intptr_t) __libcpp_mutex_lock;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_mutex_trylock;
+    gthp = (intptr_t) __libcpp_mutex_trylock;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_mutex_unlock;
+    gthp = (intptr_t) __libcpp_mutex_unlock;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_recursive_mutex_destroy;
+    gthp = (intptr_t) __libcpp_recursive_mutex_destroy;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_recursive_mutex_init;
+    gthp = (intptr_t) __libcpp_recursive_mutex_init;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_recursive_mutex_lock;
+    gthp = (intptr_t) __libcpp_recursive_mutex_lock;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_recursive_mutex_trylock;
+    gthp = (intptr_t) __libcpp_recursive_mutex_trylock;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_recursive_mutex_unlock;
+    gthp = (intptr_t) __libcpp_recursive_mutex_unlock;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_thread_create;
+    gthp = (intptr_t) __libcpp_thread_create;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_thread_detach;
+    gthp = (intptr_t) __libcpp_thread_detach;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_thread_get_current_id;
+    gthp = (intptr_t) __libcpp_thread_get_current_id;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_thread_get_id;
+    gthp = (intptr_t) __libcpp_thread_get_id;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_thread_id_equal;
+    gthp = (intptr_t) __libcpp_thread_id_equal;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_thread_id_less;
+    gthp = (intptr_t) __libcpp_thread_id_less;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_thread_isnull;
+    gthp = (intptr_t) __libcpp_thread_isnull;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_thread_join;
+    gthp = (intptr_t) __libcpp_thread_join;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_thread_yield;
+    gthp = (intptr_t) __libcpp_thread_yield;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_tls_create;
+    gthp = (intptr_t) __libcpp_tls_create;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_tls_delete;
+    gthp = (intptr_t) __libcpp_tls_delete;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_tls_get;
+    gthp = (intptr_t) __libcpp_tls_get;
+    assert(mcfp == gthp);
+
+    mcfp = (intptr_t) __MCF_libcxx_tls_set;
+    gthp = (intptr_t) __libcpp_tls_set;
+    assert(mcfp == gthp);
 
     return 0;
   }
