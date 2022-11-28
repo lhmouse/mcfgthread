dc2c975861146f2f88ddea8b44ec699f72739099
LIU Hao <lh_mouse@126.com>
2022-11-28 16:33:02 +0800

    sem: Optimize a little

--- src/sem.c
+++ src/sem.c
@@ -71,15 +71,13 @@ _MCF_sem_signal_some(_MCF_sem* sem, intptr_t value_add)
 
     _MCF_atomic_load_pptr_rlx(&old, sem);
     do {
-      if(old.__value < 0)
-        wake_num = _MCF_minz(-(size_t) old.__value, (size_t) value_add);
-      else if(value_add <= __MCF_SEM_VALUE_MAX - old.__value)
-        wake_num = 0;  /* no thread to wake up  */
-      else
+      if(old.__value > __MCF_SEM_VALUE_MAX - value_add)
         return -2;  /* would overflow  */
 
       new = old;
+      wake_num = _MCF_minz(-(size_t) old.__value, (size_t) value_add) & (size_t) (old.__value >> (__MCF_PTR_BITS - 1));
       new.__value = old.__value + value_add;
+
     }
     while(!_MCF_atomic_cmpxchg_weak_pptr_rlx(sem, &old, &new));
 
