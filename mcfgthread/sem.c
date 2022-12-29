4b73795751a2db8b7809cb16d5bb2f9a5963c77d
LIU Hao <lh_mouse@126.com>
2022-12-29 17:27:59 +0800

    sem: Fix comments
    The value of a semaphore is decreased when a thread starts waiting, and
    increased when a thread is notified. The wait counter of another type of
    object (mutex, once flag, condition variable, event) is increased when a
    thread starts waiting, and decreased when a thread is notified.
    
    (cherry picked from commit 5b5ddc919bb4994a95eb0ca30febff765112317d)
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mcfgthread/sem.c
+++ mcfgthread/sem.c
@@ -43,13 +43,13 @@ _MCF_sem_wait(_MCF_sem* sem, const int64_t* timeout_opt)
       if(old.__value < 0)
         return -1;
 
-      /* ... It is possible that a second thread has already decremented the
+      /* ... It is possible that a second thread has already incremented the
        * counter. If this does take place, it is going to release the keyed
        * event soon. We must still wait, otherwise we get a deadlock in the
        * second thread. However, a third thread could start waiting for this
        * keyed event before us, so we set the timeout to zero. If we time out
-       * once more, the third thread will have incremented the number of
-       * sleeping threads and we can try decrementing it again.  */
+       * once more, the third thread will have decremented the number of
+       * sleeping threads and we can try incrementing it again.  */
       status = __MCF_keyed_event_signal(sem, (LARGE_INTEGER[]) { 0 });
     }
 
