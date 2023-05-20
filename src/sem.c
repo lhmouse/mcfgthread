1e75dad2738f1e78aab4e216a494de32ff51e46b
LIU Hao <lh_mouse@126.com>
2023-05-20 22:27:16 +0800

    cond,mutex,once,sem,event,thread: Fix fallback paths for missed wakeups
    The comments above those lines say 'wait again' and it is
    `__MCF_keyed_event_wait()` that shall be called.
    
    This fixes #86.
    
    Signed-off-by: LIU Hao <lh_mouse@126.com>
    (cherry picked from commit d283e3f095c5c4e6a4f1c2d9ae74415cc8df85e9)
    Signed-off-by: LIU Hao <lh_mouse@126.com>
    (cherry picked from commit 5b5416a9b616c5dd00b9ad16d5990bea914d06fd)
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- src/sem.c
+++ src/sem.c
@@ -49,7 +49,7 @@ _MCF_sem_wait(_MCF_sem* sem, const int64_t* timeout_opt)
        * keyed event before us, so we set the timeout to zero. If we time out
        * once more, the third thread will have decremented the number of
        * sleeping threads and we can try incrementing it again.  */
-      status = __MCF_keyed_event_signal(sem, (LARGE_INTEGER[]) { 0 });
+      status = __MCF_keyed_event_wait(sem, (LARGE_INTEGER[]) { 0 });
     }
 
     /* We have got notified.  */
