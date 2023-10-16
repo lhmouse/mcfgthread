ac71e7223e6828e65b10853885e84f8fb5506289
LIU Hao <lh_mouse@126.com>
2023-10-16 23:36:00 +0800

    ci: Use GitHub Actions
    Closes #89.
    
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- ci/build.sh
+++ ci/build.sh
@@ -3,19 +3,31 @@
 # setup
 export CC=${CC:-"gcc"}
 export CFLAGS='-O0 -g0'
+export DISTCHECK_CONFIGURE_FLAGS="${CONFIGURE_OPTS}"
 
 # build
+if test "${CONFIGURE_BUILD}" != ""
+then
+  DISTCHECK_CONFIGURE_FLAGS="--build=${CONFIGURE_BUILD} ${DISTCHECK_CONFIGURE_FLAGS}"
+fi
+
+if test "${CONFIGURE_HOST}" != ""
+then
+  DISTCHECK_CONFIGURE_FLAGS="--host=${CONFIGURE_HOST} ${DISTCHECK_CONFIGURE_FLAGS}"
+  CC="${CONFIGURE_HOST}-gcc"
+fi
+
 ${CC} --version
 mkdir -p m4
 autoreconf -ifv
 
 cd $(mktemp -d)
 trap 'rm -rf ~+ || true' EXIT
-~-/configure --{build,host}=${CONFIGURE_HOST} ${CONFIGURE_OPTS}  \
+~-/configure ${DISTCHECK_CONFIGURE_FLAGS}  \
     --disable-dependency-tracking --disable-silent-rules --enable-debug-checks
 
 # test
-if ! make -j$(nproc) distcheck DISTCHECK_CONFIGURE_FLAGS=${CONFIGURE_OPTS}
+if ! make -j$(nproc) distcheck
 then
   find . -name 'test-suite.log' -exec cat '{}' ';'
   exit 1
