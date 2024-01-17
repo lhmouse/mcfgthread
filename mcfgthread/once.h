eae520296ba0e7f895b497c30e52b93e8795784a
LIU Hao <lh_mouse@126.com>
2024-01-17 16:21:44 +0800

    once: Remove a `__builtin_expect`

--- mcfgthread/once.h
+++ mcfgthread/once.h
@@ -101,7 +101,7 @@ _MCF_once_wait(_MCF_once* __once, const int64_t* __timeout_opt) __MCF_NOEXCEPT
 
     /* Check the first byte to see whether initialization has been completed,
      * and if that's the case, don't do anything.  */
-    if(__builtin_expect(__old.__ready, 1))
+    if(__old.__ready)
       return 0;
 
     if(__timeout_opt && (*__timeout_opt == 0) && __old.__locked)
