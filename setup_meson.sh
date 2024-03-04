85ba50a2197c837cdc94da494cd9e01b925df278
LIU Hao <lh_mouse@126.com>
2024-03-04 12:54:20 +0800

    build: Enable `-g` for release builds

--- setup_meson.sh
+++ setup_meson.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -e
 
-meson setup -Dbuildtype=debug -Denable-debug-checks=true build_debug
-meson setup -Dbuildtype=release build_release
+meson setup  \
+  -Ddebug=true -Doptimization=0  \
+  -Denable-debug-checks=true  \
+  -Db_sanitize=address,undefined  \
+  build_debug
+
+meson setup  \
+  -Ddebug=true -Doptimization=s  \
+  build_release
