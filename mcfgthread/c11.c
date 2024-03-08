33e75c63551ac5299c7906a9f0b5370294003caa
LIU Hao <lh_mouse@126.com>
2024-03-08 16:29:11 +0800

    c11: A little cleanup

--- mcfgthread/c11.c
+++ mcfgthread/c11.c
@@ -35,18 +35,17 @@ __MCF_c11_thrd_sleep(const __MCF_timespec* dur, __MCF_timespec* rem_opt)
     end_time = __builtin_fmax(end_time, 0);
     end_time = __builtin_fmin(end_time, 0x1p63 - 0x1p10);
 
+    if(rem_opt)
+      *rem_opt = (__MCF_timespec) __MCF_0_INIT;
+
     int64_t timeout = (int64_t) end_time;
     int err = _MCF_sleep(&timeout);
 
-    if(rem_opt) {
-      *rem_opt = (__MCF_timespec) __MCF_0_INIT;
-
-      if(err) {
-        /* Calculate the remaining time if the operation was interrupted.  */
-        double rem = __builtin_fmax(end_time - _MCF_hires_utc_now(), 0) * 0.001;
-        rem_opt->tv_sec = (time_t) rem;
-        rem_opt->tv_nsec = (long) ((rem - (double) rem_opt->tv_sec) * 1000000000);
-      }
+    if(rem_opt && err) {
+      /* Calculate the remaining time if the operation was interrupted.  */
+      double rem = __builtin_fmax(end_time - _MCF_hires_utc_now(), 0) * 0.001;
+      rem_opt->tv_sec = (time_t) rem;
+      rem_opt->tv_nsec = (long) ((rem - (double) rem_opt->tv_sec) * 1000000000);
     }
 
     /* Return 0 in case of timeouts, and -1 in case of interrupts.  */
