02aeb3a2aad8348d2b1cc7eb35b4c3feb80a168a
LIU Hao <lh_mouse@126.com>
2024-03-20 09:32:36 +0800

    test/cxx11_no_exceptions: New test

---
+++ test/cxx11_no_exceptions.cpp
@@ -0,0 +1,19 @@
+/* This file is part of MCF Gthread.
+ * See LICENSE.TXT for licensing information.
+ * Copyleft 2022, LH_Mouse. All wrongs reserved.  */
+
+#include "../mcfgthread/cxx11.hpp"
+
+#ifdef __EXCEPTIONS
+#  error -fno-exceptions
+#endif
+
+#ifdef __GXX_RTTI
+#  error -fno-rtti
+#endif
+
+int
+main(void)
+  {
+    return 0;
+  }
