978a4158d14988ed21ce2d08eccb8ead44289314
LIU Hao <lh_mouse@126.com>
2024-05-03 17:31:18 +0800

    test/cxx11_pedantic: New test

---
+++ test/cxx11_pedantic.cpp
@@ -0,0 +1,15 @@
+/* This file is part of MCF Gthread.
+ * See LICENSE.TXT for licensing information.
+ * Copyleft 2022, LH_Mouse. All wrongs reserved.  */
+
+#include "../mcfgthread/cxx11.hpp"
+
+#if __cplusplus < 201103L
+#  error Please compile this file as C++11.
+#endif
+
+int
+main(void)
+  {
+    return 0;
+  }
