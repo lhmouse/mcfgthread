7a004650823c14aeccc63387627d4653245f22ae
LIU Hao <lh_mouse@126.com>
2024-08-19 22:31:07 +0800

    event,sem: Fix inline definitions for C89
    (cherry picked from commit 7b5614c54ea18f656ac8534537d694a65231f71c)
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- test/gthr_c89_pedantic.c
+++ test/gthr_c89_pedantic.c
@@ -5,7 +5,24 @@
  * LICENSE.TXT as a whole. The GCC Runtime Library Exception applies
  * to this file.  */
 
+#include "../mcfgthread/atomic.h"
+#include "../mcfgthread/c11.h"
+#include "../mcfgthread/clock.h"
+#include "../mcfgthread/cond.h"
+#include "../mcfgthread/cxa.h"
+#include "../mcfgthread/dtor_queue.h"
+#include "../mcfgthread/event.h"
+#include "../mcfgthread/exit.h"
+#include "../mcfgthread/fwd.h"
 #include "../mcfgthread/gthr.h"
+#include "../mcfgthread/gthr_aux.h"
+#include "../mcfgthread/gthr_libobjc.h"
+#include "../mcfgthread/libcxx.h"
+#include "../mcfgthread/mutex.h"
+#include "../mcfgthread/once.h"
+#include "../mcfgthread/sem.h"
+#include "../mcfgthread/thread.h"
+#include "../mcfgthread/tls.h"
 
 #if defined __STDC_VERSION__ && (__STDC_VERSION__ >= 199901L)
 #  error Please compile this file as C89.
