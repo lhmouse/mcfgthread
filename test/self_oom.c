30700b56cbca3e240b94eaee1a7bd8f4f210226d
LIU Hao <lh_mouse@126.com>
2024-09-12 21:09:41 +0800

    test/self_oom: New test for OOM

---
+++ test/self_oom.c
@@ -0,0 +1,55 @@
+/* This file is part of MCF Gthread.
+ * Copyright (C) 2022-2024 LH_Mouse. All wrongs reserved.
+ *
+ * MCF Gthread is free software. Licensing information is included in
+ * LICENSE.TXT as a whole. The GCC Runtime Library Exception applies
+ * to this file.  */
+
+#include "../mcfgthread/xglobals.h"
+#include <assert.h>
+#include <stdio.h>
+#include <windows.h>
+
+#define NTHREADS  64U
+static HANDLE threads[NTHREADS];
+
+static
+DWORD
+__stdcall
+thread_proc(void* arg)
+  {
+    (void) arg;
+    Sleep(1000);
+    assert(_MCF_thread_self() == &(__MCF_g->__thread_oom_self_st));
+    Sleep(100);
+    printf("thread %d quitting\n", _MCF_thread_self_tid());
+    return 0;
+  }
+
+int
+main(void)
+  {
+#if defined __CYGWIN__
+    return 77;
+#else
+    // replace global heap
+    *(HANDLE*) &__MCF_crt_heap = HeapCreate(0, 0, 1048576);  // 1 MB
+    assert(__MCF_crt_heap);
+
+    // allocate until oom
+    while(__MCF_malloc_0(100) != __MCF_nullptr)
+      continue;
+
+    for(size_t k = 0;  k < NTHREADS;  ++k) {
+      threads[k] = CreateThread(NULL, 0, thread_proc, NULL, 0, NULL);
+      assert(threads[k]);
+    }
+
+    printf("main waiting\n");
+    for(intptr_t k = 0;  k < NTHREADS;  ++k) {
+      WaitForSingleObject(threads[k], INFINITE);
+      CloseHandle(threads[k]);
+      printf("main wait finished: %d\n", (int)k);
+    }
+#endif
+  }
