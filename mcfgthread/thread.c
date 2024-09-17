187a40d5a8bde21f5df2412ee1e2f9f60f736e2d
LIU Hao <lh_mouse@126.com>
2024-09-18 02:05:23 +0800

    thread: Optimize `_MCF_thread_self()` for small TLS indexes

--- mcfgthread/thread.c
+++ mcfgthread/thread.c
@@ -193,7 +193,24 @@ __MCF_DLLEXPORT
 _MCF_thread*
 _MCF_thread_self(void)
   {
-    _MCF_thread* self = TlsGetValue(__MCF_g->__tls_index);
+    _MCF_thread* self = __MCF_nullptr;
+
+    /* Try fetching the pointer from TEB to save an indirect function call.  */
+    if(__MCF_g->__tls_index < 64) {
+      uintptr_t index = __MCF_g->__tls_index;
+#if defined __amd64__
+      __asm__ ("mov %0, gs:[0x1480 + %1 * 8]" : "=r"(self) : "r"(index));
+#elif defined __i386__
+      __asm__ ("mov %0, fs:[0xE10 + %1 * 4]" : "=r"(self) : "r"(index));
+#elif defined __aarch64__
+      __asm__ ("ldr %0, [x18, %1, lsl 3]" : "=r"(self) : "r"(0x1480 / 8 + index));
+#endif
+      if(self)
+        return self;
+    }
+
+    /* Try the generic path.  */
+    self = TlsGetValue(__MCF_g->__tls_index);
     if(self)
       return self;
 
