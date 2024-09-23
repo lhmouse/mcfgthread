74c5e2367aa6401f91e6cb8b94eae371fcbd9063
LIU Hao <lh_mouse@126.com>
2024-09-23 17:01:16 +0800

    test: Fix comments

--- test/gthr_cond_timeout.c
+++ test/gthr_cond_timeout.c
@@ -26,7 +26,7 @@ main(void)
     r = __gthread_mutex_trylock(&mutex);
     assert(r == 0);
 
-    /* Round the time up. The `while` loops is necessary to work around a bug
+    /* Round the time up. The `while` loop is necessary to work around a bug
      * in Wine, which physical Windows doesn't have.
      * See https://bugs.winehq.org/show_bug.cgi?id=57035  */
     int64_t sleep_until = (int64_t) time(__MCF_nullptr) * 1000 + 2000;
