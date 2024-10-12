c9a4bb5e6fafccec45463efbc1b8169d8631e22e
LIU Hao <lh_mouse@126.com>
2024-10-12 10:51:06 +0800

    Z

---
+++ mcfgthread/shared_mutex.c
@@ -0,0 +1,162 @@
+/* This file is part of MCF Gthread.
+ * Copyright (C) 2022-2024 LH_Mouse. All wrongs reserved.
+ *
+ * MCF Gthread is free software. Licensing information is included in
+ * LICENSE.TXT as a whole. The GCC Runtime Library Exception applies
+ * to this file.  */
+
+#include "xprecompiled.h"
+#define __MCF_SHARED_MUTEX_IMPORT  __MCF_DLLEXPORT
+#define __MCF_SHARED_MUTEX_INLINE  __MCF_DLLEXPORT
+#include "mutex.h"
+#include "xglobals.h"
+
+__MCF_DLLEXPORT
+int
+_MCF_mutex_lock_slow(_MCF_mutex* mutex, const int64_t* timeout_opt)
+  {
+    __MCF_winnt_timeout nt_timeout;
+    __MCF_initialize_winnt_timeout_v3(&nt_timeout, timeout_opt);
+
+    /* If this mutex has not been locked, lock it; otherwise, if `__sp_mask`
+     * is less than `__MCF_SHARED_MUTEX_SP_MASK_M` and `__sp_nfail` is less than
+     * `__MCF_SHARED_MUTEX_SP_NFAIL_THRESHOLD`, which means the current thread is
+     * allowed to spin, allocate a spinning bit; otherwise, allocate a
+     * sleeping count. The spinning failure counter is decremented if the
+     * mutex can be locked immediately; and incremented otherwise, no matter
+     * whether the current thread is going to spin or not.  */
+    _MCF_mutex old, new;
+    uint32_t spinnable;
+  try_lock_loop:
+    _MCF_atomic_load_pptr_rlx(&old, mutex);
+    do {
+      new = old;
+      new.__locked = 1;
+      spinnable = (uint32_t) ((old.__sp_mask - __MCF_SHARED_MUTEX_SP_MASK_M) & (old.__sp_nfail - __MCF_SHARED_MUTEX_SP_NFAIL_THRESHOLD)) >> 31;
+      new.__sp_mask = (old.__sp_mask | (old.__sp_mask + (old.__locked & spinnable))) & __MCF_SHARED_MUTEX_SP_MASK_M;
+      new.__sp_nfail = do_adjust_sp_nfail(old.__sp_nfail, (int) old.__locked * 2 - 1) & __MCF_SHARED_MUTEX_SP_NFAIL_M;
+      new.__nsleep = (old.__nsleep + (old.__locked & ~spinnable)) & __MCF_SHARED_MUTEX_NSLEEP_M;
+    }
+    while(!_MCF_atomic_cmpxchg_weak_pptr_arl(mutex, &old, &new));
+
+    /* If this mutex has been locked by the current thread, succeed.  */
+    if(old.__locked == 0)
+      return 0;
+
+    uint32_t my_mask = (uint32_t) (old.__sp_mask ^ new.__sp_mask);
+    if(my_mask != 0) {
+      __MCF_ASSERT((my_mask & (my_mask - 1U)) == 0);
+
+      /* Calculate the spin count for this loop.  */
+      register int spin = (int) (__MCF_SHARED_MUTEX_SP_NFAIL_THRESHOLD - old.__sp_nfail);
+      __MCF_ASSERT(spin > 0);
+      spin *= (int) (__MCF_SHARED_MUTEX_MAX_SPIN_COUNT / __MCF_SHARED_MUTEX_SP_NFAIL_THRESHOLD);
+
+      while(__builtin_expect(spin, 100) > 0) {
+        spin --;
+        YieldProcessor();
+
+        /* Wait for my turn.  */
+        BYTE cmp = 1;
+        if(!_MCF_atomic_cmpxchg_weak_8_rlx(do_spin_byte_ptr(mutex, my_mask), (int8_t*) &cmp, 0))
+          continue;
+
+        /* If this mutex has not been locked, lock it and decrement the
+         * failure counter. Otherwise, do nothing.  */
+        _MCF_atomic_load_pptr_rlx(&old, mutex);
+        do {
+          if(old.__locked != 0)
+            break;
+
+          new = old;
+          new.__locked = 1;
+          new.__sp_nfail = do_adjust_sp_nfail(old.__sp_nfail, -1) & __MCF_SHARED_MUTEX_SP_NFAIL_M;
+        }
+        while(!_MCF_atomic_cmpxchg_weak_pptr_acq(mutex, &old, &new));
+
+        /* If this mutex has been locked by the current thread, succeed.  */
+        if(old.__locked == 0)
+          return 0;
+      }
+
+      /* We have wasted some time. Now allocate a sleeping count.
+       * IMPORTANT! We can increment the sleeping counter ONLY IF the mutex
+       * is being locked by another thread. Otherwise, if the other thread
+       * had unlocked the mutex before we incremented the sleeping counter,
+       * we could miss a wakeup and result in deadlocks.  */
+      _MCF_atomic_load_pptr_rlx(&old, mutex);
+      do {
+        new = old;
+        new.__locked = 1;
+        new.__sp_nfail = do_adjust_sp_nfail(old.__sp_nfail, (int) old.__locked - 1) & __MCF_SHARED_MUTEX_SP_NFAIL_M;
+        new.__nsleep = (old.__nsleep + old.__locked) & __MCF_SHARED_MUTEX_NSLEEP_M;
+      }
+      while(!_MCF_atomic_cmpxchg_weak_pptr_acq(mutex, &old, &new));
+
+      /* If this mutex has been locked by the current thread, succeed.  */
+      if(old.__locked == 0)
+        return 0;
+    }
+
+    /* Try waiting.  */
+    int err = __MCF_keyed_event_wait(mutex, &nt_timeout);
+    while(err != 0) {
+      /* Tell another thread which is going to signal this mutex that an old
+       * waiter has left by decrementing the number of sleeping threads. But
+       * see below...  */
+      _MCF_atomic_load_pptr_rlx(&old, mutex);
+      do {
+        if(old.__nsleep == 0)
+          break;
+
+        new = old;
+        new.__nsleep = (old.__nsleep - 1U) & __MCF_SHARED_MUTEX_NSLEEP_M;
+      }
+      while(!_MCF_atomic_cmpxchg_weak_pptr_rlx(mutex, &old, &new));
+
+      if(old.__nsleep != 0)
+        return -1;
+
+      /* ... It is possible that a second thread has already decremented the
+       * counter. If this does take place, it is going to release the keyed
+       * event soon. We must still wait, otherwise we get a deadlock in the
+       * second thread. However, a third thread could start waiting for this
+       * keyed event before us, so we set the timeout to zero. If we time out
+       * once more, the third thread will have incremented the number of
+       * sleeping threads and we can try decrementing it again.  */
+      err = __MCF_keyed_event_wait(mutex, &(__MCF_winnt_timeout) { 0 });
+    }
+
+    /* We have got notified.  */
+    __MCF_adjust_winnt_timeout_v3(&nt_timeout);
+    goto try_lock_loop;
+  }
+
+__MCF_DLLEXPORT __MCF_NEVER_INLINE
+void
+_MCF_mutex_unlock_slow(_MCF_mutex* mutex)
+  {
+    /* Clear the `__locked` field and release at most one thread, if any.
+     * The right most bit one of the spinning mask is also cleared to enable
+     * further threads to spin.  */
+    size_t wake_one;
+    _MCF_mutex old, new;
+
+    _MCF_atomic_load_pptr_rlx(&old, mutex);
+    do {
+      new = old;
+      new.__locked = 0;
+      new.__sp_mask = (old.__sp_mask & (old.__sp_mask - 1U)) & __MCF_SHARED_MUTEX_SP_MASK_M;
+      wake_one = _MCF_minz(old.__nsleep, 1);
+      new.__nsleep = (old.__nsleep - wake_one) & __MCF_SHARED_MUTEX_NSLEEP_M;
+    }
+    while(!_MCF_atomic_cmpxchg_weak_pptr_rel(mutex, &old, &new));
+
+    /* Notify a spinning thread, if any. If `__sp_mask` was non-zero, only its
+     * rightmost bit should have been cleared, so we need not calculate the
+     * bit difference, unlike `_MCF_mutex_lock_slow()`.  */
+    if(old.__sp_mask != 0)
+      _MCF_atomic_store_8_rlx(do_spin_byte_ptr(mutex, old.__sp_mask), 1);
+
+    __MCF_batch_release_common(mutex, wake_one);
+  }
---
+++ mcfgthread/shared_mutex.h
@@ -0,0 +1,133 @@
+/* This file is part of MCF Gthread.
+ * Copyright (C) 2022-2024 LH_Mouse. All wrongs reserved.
+ *
+ * MCF Gthread is free software. Licensing information is included in
+ * LICENSE.TXT as a whole. The GCC Runtime Library Exception applies
+ * to this file.  */
+
+#ifndef __MCFGTHREAD_SHARED_MUTEX_
+#define __MCFGTHREAD_SHARED_MUTEX_
+
+#include "fwd.h"
+#include "atomic.h"
+
+__MCF_C_DECLARATIONS_BEGIN
+#ifndef __MCF_SHARED_MUTEX_IMPORT
+#  define __MCF_SHARED_MUTEX_IMPORT
+#  define __MCF_SHARED_MUTEX_INLINE  __MCF_GNU_INLINE
+#endif
+
+/* Define the shared mutex struct.
+ * This takes up the same storage as a pointer.  */
+struct __MCF_shared_mutex
+  {
+#define __MCF_SHARED_MUTEX_NSHARE_M  16383U
+    __MCF_EX uintptr_t __nshare : 14;  /* number of sharing threads  */
+
+#define __MCF_SHARED_MUTEX_NSLEEP_M  (__MCF_UPTR_MAX >> 14)
+    __MCF_EX uintptr_t __nsleep : __MCF_PTR_BITS - 14;  /* number of sleeping threads  */
+  };
+
+/* This special value indicates that the shared mutex has been locked in
+ * exclusive mode.  */
+#define __MCF_SHARED_MUTEX_NSHARE_EXCLUSIVE   16382U
+
+/* Initializes a shared mutex dynamically.
+ * Static ones should be initialized with `{0}`, like other structs.  */
+__MCF_SHARED_MUTEX_INLINE
+void
+_MCF_shared_mutex_init(_MCF_shared_mutex* __mutex) __MCF_NOEXCEPT;
+
+/* Attempts
+
+
+
+
+
+
+
+
+
+/* Attempts to lock a mutex.
+ * This a simple mutex that is not recursive and performs no error checking. If
+ * the caller attempts to lock a mutex which it has already held, deadlocks may
+ * occur.
+ *
+ * If the `__timeout_opt` argument points to a positive integer, it denotes the
+ * expiration time in number of milliseconds since 1970-01-01T00:00:00Z. If it
+ * points to a negative integer, the absolute value of it denotes the number of
+ * milliseconds to wait. If it points to zero, the function returns immediately
+ * without waiting. If it is null, the function waits indefinitely.
+ *
+ * Returns 0 if the mutex has been locked by the caller, or -1 if the operation
+ * has timed out.  */
+__MCF_SHARED_MUTEX_IMPORT
+int
+_MCF_mutex_lock_slow(_MCF_mutex* __mutex, const int64_t* __timeout_opt) __MCF_NOEXCEPT;
+
+__MCF_SHARED_MUTEX_INLINE
+int
+_MCF_mutex_lock(_MCF_mutex* __mutex, const int64_t* __timeout_opt) __MCF_NOEXCEPT;
+
+/* Releases a mutex. This function may be called by a different thread from
+ * which locked the same mutex. If the mutex has not been locked, the behavior
+ * is undefined.  */
+__MCF_SHARED_MUTEX_IMPORT
+void
+_MCF_mutex_unlock_slow(_MCF_mutex* __mutex) __MCF_NOEXCEPT;
+
+__MCF_SHARED_MUTEX_INLINE
+void
+_MCF_mutex_unlock(_MCF_mutex* __mutex) __MCF_NOEXCEPT;
+
+/* Define inline functions after all declarations.
+ * We would like to keep them away from declarations for conciseness, which also
+ * matches the disposition of non-inline functions. Note that however, unlike C++
+ * inline functions, they have to have consistent inline specifiers throughout
+ * this file.  */
+__MCF_SHARED_MUTEX_INLINE
+void
+_MCF_mutex_init(_MCF_mutex* __mutex) __MCF_NOEXCEPT
+  {
+    _MCF_mutex __temp = __MCF_0_INIT;
+    _MCF_atomic_store_pptr_rel(__mutex, &__temp);
+  }
+
+__MCF_SHARED_MUTEX_INLINE
+int
+_MCF_mutex_lock(_MCF_mutex* __mutex, const int64_t* __timeout_opt) __MCF_NOEXCEPT
+  {
+#if defined __OPTIMIZE__ && !defined __OPTIMIZE_SIZE__
+    _MCF_mutex __old = { 0, 0, 0, 0 };
+    _MCF_mutex __new = { 1, 0, 0, 0 };
+
+    /* This is optimized solely for single-thread code.  */
+    if(_MCF_atomic_cmpxchg_weak_pptr_acq(__mutex, &__old, &__new))
+      return 0;
+
+    /* If a timeout of zero is specified, don't block at all.  */
+    if(__timeout_opt && (*__timeout_opt == 0) && __old.__locked)
+      return -1;
+#endif  /* speed */
+
+    return _MCF_mutex_lock_slow(__mutex, __timeout_opt);
+  }
+
+__MCF_SHARED_MUTEX_INLINE
+void
+_MCF_mutex_unlock(_MCF_mutex* __mutex) __MCF_NOEXCEPT
+  {
+#if defined __OPTIMIZE__ && !defined __OPTIMIZE_SIZE__
+    _MCF_mutex __old = { 1, 0, 0, 0 };
+    _MCF_mutex __new = { 0, 0, 0, 0 };
+
+    /* This is optimized solely for single-thread code.  */
+    if(_MCF_atomic_cmpxchg_weak_pptr_rel(__mutex, &__old, &__new))
+      return;
+#endif  /* speed */
+
+    _MCF_mutex_unlock_slow(__mutex);
+  }
+
+__MCF_C_DECLARATIONS_END
+#endif  /* __MCFGTHREAD_SHARED_MUTEX_  */
