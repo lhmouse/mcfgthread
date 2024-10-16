a6844426887508f7fbceb11ace77a7112408acc8
LIU Hao <lh_mouse@126.com>
2024-10-16 23:18:43 +0800

    mutex: Don't update `__sp_mask` when unlocking a mutex
    It's now updated in the locking function.

--- mcfgthread/mutex.c
+++ mcfgthread/mutex.c
@@ -189,7 +189,7 @@ _MCF_mutex_unlock_slow(_MCF_mutex* mutex)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wconversion"
       new.__locked = 0;
-      new.__sp_mask = old.__sp_mask & (old.__sp_mask - 1U);
+      new.__sp_mask = old.__sp_mask;
       new.__sp_nfail = old.__sp_nfail;
       new.__nsleep = old.__nsleep - wake_one;
 #pragma GCC diagnostic pop
@@ -197,8 +197,7 @@ _MCF_mutex_unlock_slow(_MCF_mutex* mutex)
     while(!_MCF_atomic_cmpxchg_weak_pptr_rel(mutex, &old, &new));
 
     /* Notify a spinning thread, if any. If `__sp_mask` was non-zero, only its
-     * rightmost bit should have been cleared, so we need not calculate the
-     * bit difference, unlike `_MCF_mutex_lock_slow()`.  */
+     * rightmost bit is of interest, so we need not calculate the difference.  */
     if(old.__sp_mask != 0)
       _MCF_atomic_store_8_rlx(do_spin_byte_ptr(mutex, old.__sp_mask), 1);
 
