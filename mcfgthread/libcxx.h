49ac20c298cf22659c536b07f6f8428f0a7262f6
LIU Hao <lh_mouse@126.com>
2024-10-26 23:24:21 +0800

    c11,gthr,libcxx: Fix headers for MSVC
    (cherry picked from commit da0826e46eaf3e39b095ef12fb41255342ed75c7)
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mcfgthread/libcxx.h
+++ mcfgthread/libcxx.h
@@ -344,7 +344,7 @@ __MCF_LIBCXX_INLINE
 int
 __MCF_libcxx_execute_once(__libcpp_exec_once_flag* __once, __MCF_once_callback* __init_proc) __MCF_MAY_THROW
   {
-    __MCF_gthr_call_once_seh(__once, __init_proc, __MCF_nullptr);
+    __MCF_gthr_call_once_seh(__once, __MCF_CAST_PTR(__MCF_cxa_dtor_cdecl, __init_proc), __MCF_nullptr);
     return 0;
   }
 
