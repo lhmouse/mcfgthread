ada3b624a25a376192d9b05a38384d6c2d6645e1
LIU Hao <lh_mouse@126.com>
2024-10-30 20:47:26 +0800

    fwd: Add # before immediates in ARM assembly

--- mcfgthread/fwd.h
+++ mcfgthread/fwd.h
@@ -303,12 +303,12 @@ __MCF_CXX(extern "C" {)
 
 #  define __MCF_TEB_LOAD_32_INDEXED(out, offset, index)  \
       __asm__ volatile (  \
-         "ldr %w0, [x18, %x1, lsl 2]"  \
+         "ldr %w0, [x18, %x1, lsl #2]"  \
           : "=r"(*(out)) : "r"((offset) / 4U + (index)))
 
 #  define __MCF_TEB_STORE_32_INDEXED(offset, index, value)  \
       __asm__ volatile (  \
-         "str %w0, [x18, %x1, lsl 2]"  \
+         "str %w0, [x18, %x1, lsl #2]"  \
           : : "r"(value), "r"((offset) / 4U + (index)))
 
 #  define __MCF_TEB_LOAD_PTR_IMMEDIATE(out, offset)  \
@@ -323,12 +323,12 @@ __MCF_CXX(extern "C" {)
 
 #  define __MCF_TEB_LOAD_PTR_INDEXED(out, offset, index)  \
       __asm__ volatile (  \
-         "ldr %0, [x18, %x1, lsl 3]"  \
+         "ldr %0, [x18, %x1, lsl #3]"  \
           : "=r"(*(out)) : "r"((offset) / 8U + (index)))
 
 #  define __MCF_TEB_STORE_PTR_INDEXED(offset, index, value)  \
       __asm__ volatile (  \
-         "str %0, [x18, %x1, lsl 3]"  \
+         "str %0, [x18, %x1, lsl #3]"  \
           : : "r"(value), "r"((offset) / 8U + (index)))
 
 #  define __MCF_64_32(x, y)  x
