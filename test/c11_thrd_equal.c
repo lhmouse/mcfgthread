d5f907209e8e8e076c19b95d28cae4e100002cfc
LIU Hao <lh_mouse@126.com>
2025-03-06 23:41:09 +0800

    test/{c11_thrd,gthr,libcxx_thread_id}_equal: Fix race conditions
    (cherry picked from commit 72203b91cad7d44fcc86ca334fe0047c4319b334)
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- test/c11_thrd_equal.c
+++ test/c11_thrd_equal.c
@@ -10,12 +10,14 @@
 #include <stdio.h>
 
 static thrd_t thrd;
+static _MCF_mutex mtx = __MCF_0_INIT;
 
 static
 int
 thread_proc(void* param)
   {
     (void) param;
+    _MCF_mutex_lock(&mtx, __MCF_nullptr);
     assert(thrd_equal(thrd_current(), thrd));
 
     fprintf(stderr, "thread %d quitting\n", (int) _MCF_thread_self_tid());
@@ -25,7 +27,9 @@ thread_proc(void* param)
 int
 main(void)
   {
+    _MCF_mutex_lock(&mtx, __MCF_nullptr);
     int r = thrd_create(&thrd, thread_proc, __MCF_nullptr);
+    _MCF_mutex_unlock(&mtx);
     assert(r == 0);
     assert(thrd);
 
