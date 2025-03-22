8456b7064e3b7c6667852d134ca78ea3c16a476c
LIU Hao <lh_mouse@126.com>
2025-03-22 19:37:35 +0800

    test/gthr_call_once_exceptional: New test

---
+++ test/gthr_call_once_exceptional.cpp
@@ -0,0 +1,80 @@
+/* This file is part of MCF Gthread.
+ * Copyright (C) 2022-2025 LH_Mouse. All wrongs reserved.
+ *
+ * MCF Gthread is free software. Licensing information is included in
+ * LICENSE.TXT as a whole. The GCC Runtime Library Exception applies
+ * to this file.  */
+
+#if !defined __SEH__ && !defined _MSC_VER
+int
+main(void)
+  {
+    return 77;
+  }
+#else  // __SEH__
+
+#include "../mcfgthread/cxx11.hpp"
+#include "../mcfgthread/sem.h"
+#include <assert.h>
+#include <stdio.h>
+#include <vector>
+
+#ifdef TEST_STD
+#  include <mutex>
+#  include <thread>
+namespace NS = std;
+#else
+namespace NS = ::_MCF;
+#endif
+
+constexpr std::size_t NTHREADS = 64U;
+static std::vector<NS::thread> threads(NTHREADS);
+static _MCF_once once;
+static ::_MCF_sem start = __MCF_SEM_INIT(NTHREADS);
+static int resource = 0;
+
+static
+void
+once_do_it(void* add)
+  {
+    /* Perform initialization.  */
+    int old = resource;
+    NS::this_thread::sleep_for(NS::chrono::milliseconds(20));
+    resource = old + (int)(intptr_t) add;
+
+    NS::this_thread::sleep_for(NS::chrono::milliseconds(10));
+    ::fprintf(stderr, "thread %d done\n", (int) ::_MCF_thread_self_tid());
+    throw 42;
+  }
+
+static
+void
+thread_proc()
+  {
+    ::_MCF_sem_wait(&start, nullptr);
+
+    try {
+      __MCF_GTHR_CALL_ONCE_SEH(&once, once_do_it, (void*) 1);
+      ::std::terminate();
+    }
+    catch(...) {  }
+
+    ::fprintf(stderr, "thread %d quitting\n", (int) ::_MCF_thread_self_tid());
+  }
+
+int
+main(void)
+  {
+    for(auto& thr : threads)
+      thr = NS::thread(thread_proc);
+
+    ::fprintf(stderr, "main waiting\n");
+    ::_MCF_sem_signal_some(&start, NTHREADS);
+
+    for(auto& thr : threads)
+      thr.join();
+
+    assert(resource == NTHREADS);
+  }
+
+#endif  // __SEH__
