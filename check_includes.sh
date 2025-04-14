27bcc8d81b7e6b81dd8291e93128ecad5f59d69b
LIU Hao <lh_mouse@126.com>
2025-04-14 15:50:50 +0800

    doc: Run inclusion checks in parallen with `xargs`
    (cherry picked from commit 47dd486661083af7d5cea9b63ab2e5c82df91fb6)
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- check_includes.sh
+++ check_includes.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -e
 
-# setup
-export CFLAGS="-D_WIN32_WINNT=0x0601 -Ibuild_debug -std=c99"
-export CXXFLAGS="-D_WIN32_WINNT=0x0601 -Ibuild_debug -std=c++11"
-
 if test -z ${CC}
 then
   if grep -Eoi '^(mingw|msys|cygwin)' /proc/version &>/dev/null
@@ -13,12 +9,6 @@ then
     CC=x86_64-w64-mingw32-gcc
   fi
 fi
-
-for _f in $(find ~+/"mcfgthread" -name "*.[hc]")
-do
-  echo "Checking includes:  ${_f}"
-  ${CC} ${CFLAGS} -x c ${_f} -S -o /dev/null
-done
 
 if test -z ${CXX}
 then
@@ -30,8 +20,13 @@ then
   fi
 fi
 
-for _f in $(find ~+/"mcfgthread" -name "*.[hc]pp")
-do
-  echo "Checking includes:  ${_f}"
-  ${CXX} ${CXXFLAGS} -x c++ ${_f} -S -o /dev/null
-done
+export CPPFLAGS="-D_WIN32_WINNT=0x0601 -Ibuild_debug"
+export CC CXX
+
+find ~+/"mcfgthread" -name "*.[hc]"  \
+  | xargs -n 1 -P $(nproc) --  \
+    sh -xec '${CC} ${CPPFLAGS} -x c -std=c99 $1 -S -o /dev/null' 0
+
+find ~+/"mcfgthread" -name "*.[hc]pp"  \
+  | xargs -n 1 -P $(nproc) --  \
+    sh -xec '${CXX} ${CPPFLAGS} -x c++ -std=c++11 $1 -S -o /dev/null' 0
