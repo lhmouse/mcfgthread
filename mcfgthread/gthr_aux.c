c3d4f37c0ee3af3fa8e57e21fd8a17c3cd399684
LIU Hao <lh_mouse@126.com>
2025-12-08 11:09:53 +0800

    gthr_aux,xglobals: Move `\n` into beginning of line

--- mcfgthread/gthr_aux.c
+++ mcfgthread/gthr_aux.c
@@ -23,9 +23,9 @@ __MCF_gthr_call_once_seh_take_over(_MCF_once* once, __MCF_cxa_dtor_any_ init_pro
   }
 
 __asm__ (
-".section .text$" __MCF_USYM "__MCF_gthr_call_once_seh_take_over, \"x\"  \n"
-".def " __MCF_USYM "do_call_once_seh_take_over; .scl 3; .type 32; .endef  \n"
-__MCF_USYM "do_call_once_seh_take_over:  \n"
+"\n .section .text$" __MCF_USYM "__MCF_gthr_call_once_seh_take_over, \"x\""
+"\n .def " __MCF_USYM "do_call_once_seh_take_over; .scl 3; .type 32; .endef"
+"\n " __MCF_USYM "do_call_once_seh_take_over:"
 #if defined __i386__
 /* On x86-32, SEH is stack-based. The stack is used as follows:
  *
@@ -41,54 +41,54 @@ __MCF_USYM "do_call_once_seh_take_over:  \n"
  *     12: `init_proc`
  *     16: `arg`
  */
-"  push ebp  \n"
-"  mov ebp, esp  \n"
-"  push esi  \n"
-"  sub esp, 20  \n"
+"\n   push ebp"
+"\n   mov ebp, esp"
+"\n   push esi"
+"\n   sub esp, 20"
 /* Initialize the constant zero.  */
-"  xor esi, esi  \n"
+"\n   xor esi, esi"
 /* Install an SEH handler.  */
-"  mov eax, DWORD PTR fs:[esi]  \n"
-"  lea ecx, [ebp - 16]  \n"
-"  mov DWORD PTR [ecx], eax  \n"
-"  mov DWORD PTR [ecx + 4], OFFSET _do_i386_call_once_on_except  \n"
-"  mov DWORD PTR fs:[esi], ecx  \n"
+"\n   mov eax, DWORD PTR fs:[esi]"
+"\n   lea ecx, [ebp - 16]"
+"\n   mov DWORD PTR [ecx], eax"
+"\n   mov DWORD PTR [ecx + 4], OFFSET _do_i386_call_once_on_except"
+"\n   mov DWORD PTR fs:[esi], ecx"
 /* Make the call `(*init_proc) (arg)`. The argument is passed both via the
  * ECX register and on the stack, to allow both `__cdecl` and `__thiscall`
  * functions to work properly.  */
-"  mov ecx, DWORD PTR [ebp + 16]  \n"
-"  mov DWORD PTR [ebp - 24], ecx  \n"
-"  call DWORD PTR [ebp + 12]  \n"
+"\n   mov ecx, DWORD PTR [ebp + 16]"
+"\n   mov DWORD PTR [ebp - 24], ecx"
+"\n   call DWORD PTR [ebp + 12]"
 /* Dismantle the SEH handler.  */
-"  mov eax, DWORD PTR [ebp - 16]  \n"
-"  mov DWORD PTR fs:[esi], eax  \n"
+"\n   mov eax, DWORD PTR [ebp - 16]"
+"\n   mov DWORD PTR fs:[esi], eax"
 /* Disarm the once flag with a tail call.  */
-"  lea esp, [ebp - 4]  \n"
-"  pop esi  \n"
-"  pop ebp  \n"
-"  jmp __MCF_once_release  \n"
+"\n   lea esp, [ebp - 4]"
+"\n   pop esi"
+"\n   pop ebp"
+"\n   jmp __MCF_once_release"
 /* Define the exception handler, which is called either when an exception is
  * raised, or the stack is being unwound.  */
-".def _do_i386_call_once_on_except; .scl 3; .type 32; .endef  \n"
-"_do_i386_call_once_on_except:  \n"
+"\n .def _do_i386_call_once_on_except; .scl 3; .type 32; .endef"
+"\n _do_i386_call_once_on_except:"
 /* Check whether `ExceptionFlags` contains `EXCEPTION_UNWINDING`.  */
-"  mov ecx, DWORD PTR [esp + 4]  \n"
-"  test BYTE PTR [ecx + 4], 2  \n"
-"  jz 1001f  \n"
+"\n   mov ecx, DWORD PTR [esp + 4]"
+"\n   test BYTE PTR [ecx + 4], 2"
+"\n   jz 1001f"
 /* Locate the once flag from `EstablisherFrame`, and reset it.  */
-"  mov eax, DWORD PTR [esp + 8]  \n"
-"  push DWORD PTR [eax + 24]  \n"
-"  call __MCF_once_abort  \n"
-"  pop eax  \n"
-"1001:  \n"
+"\n   mov eax, DWORD PTR [esp + 8]"
+"\n   push DWORD PTR [eax + 24]"
+"\n   call __MCF_once_abort"
+"\n   pop eax"
+"\n 1001:"
 /* Return `ExceptionContinueSearch`.  */
-"  mov eax, 1  \n"
-"  ret  \n"
+"\n   mov eax, 1"
+"\n   ret"
 #  if defined __MCF_IN_DLL
-".globl ___MCF_gthr_do_i386_call_once_on_except  \n"
-".equiv ___MCF_gthr_do_i386_call_once_on_except, _do_i386_call_once_on_except  \n"
+"\n .globl ___MCF_gthr_do_i386_call_once_on_except"
+"\n .equiv ___MCF_gthr_do_i386_call_once_on_except, _do_i386_call_once_on_except"
 #  elif defined _MSC_VER
-".safeseh _do_i386_call_once_on_except  \n"
+"\n .safeseh _do_i386_call_once_on_except"
 #  endif
 #elif defined __amd64__ && !defined __arm64ec__
 /* On x86-64, SEH is table-based. We register an unwind handler which is not
@@ -106,38 +106,38 @@ __MCF_USYM "do_call_once_seh_take_over:  \n"
  *     32: shadow slot for `arg` from R8
  *     40: unused
  */
-".seh_proc do_call_once_seh_take_over  \n"
-".seh_handler do_amd64_call_once_on_unwind, @unwind  \n"
-"  push rbp  \n"
-".seh_pushreg rbp  \n"
-"  mov rbp, rsp  \n"
-".seh_setframe rbp, 0  \n"
-"  sub rsp, 32  \n"
-".seh_stackalloc 32  \n"
-".seh_endprologue  \n"
+"\n .seh_proc do_call_once_seh_take_over"
+"\n .seh_handler do_amd64_call_once_on_unwind, @unwind"
+"\n   push rbp"
+"\n .seh_pushreg rbp"
+"\n   mov rbp, rsp"
+"\n .seh_setframe rbp, 0"
+"\n   sub rsp, 32"
+"\n .seh_stackalloc 32"
+"\n .seh_endprologue"
 /* Stash `once` for the handler.  */
-"  mov QWORD PTR [rbp + 16], rcx  \n"
+"\n   mov QWORD PTR [rbp + 16], rcx"
 /* Make the call `(*init_proc) (arg)`.  */
-"  mov rcx, r8  \n"
-"  call rdx  \n"
+"\n   mov rcx, r8"
+"\n   call rdx"
 /* Disarm the once flag with a tail call. The x64 stack unwinder recognizes
  * `add rsp, SIZE` as the start of the epilogue.  */
-"  mov rcx, QWORD PTR [rbp + 16]  \n"
-"  add rsp, 32  \n"
-"  pop rbp  \n"
-"  jmp _MCF_once_release  \n"
-".seh_endproc  \n"
+"\n   mov rcx, QWORD PTR [rbp + 16]"
+"\n   add rsp, 32"
+"\n   pop rbp"
+"\n   jmp _MCF_once_release"
+"\n .seh_endproc"
 /* Define the unwind handler, which is called the stack is being unwound.  */
-".def do_amd64_call_once_on_unwind; .scl 3; .type 32; .endef  \n"
-"do_amd64_call_once_on_unwind:  \n"
-"  sub rsp, 40  \n"
+"\n .def do_amd64_call_once_on_unwind; .scl 3; .type 32; .endef"
+"\n do_amd64_call_once_on_unwind:"
+"\n   sub rsp, 40"
 /* Locate the once flag from `EstablisherFrame`, and reset it.  */
-"  mov rcx, QWORD PTR [rdx + 16]  \n"
-"  call _MCF_once_abort  \n"
+"\n   mov rcx, QWORD PTR [rdx + 16]"
+"\n   call _MCF_once_abort"
 /* Return `ExceptionContinueSearch`.  */
-"  mov eax, 1  \n"
-"  add rsp, 40  \n"
-"  ret  \n"
+"\n   mov eax, 1"
+"\n   add rsp, 40"
+"\n   ret"
 #elif defined __aarch64__ || defined __arm64ec__
 /* On ARM64, SEH is table-based. Unlike x86-64 but like x86-32, there is only
  * one kind of handler which is called in either case. The stack is used as
@@ -149,49 +149,49 @@ __MCF_USYM "do_call_once_seh_take_over:  \n"
  *     24: unused
  * CFA 32: establisher frame
  */
-".seh_proc do_call_once_seh_take_over  \n"
-".seh_handler do_arm64_call_once_on_except, @except  \n"
-"  stp fp, lr, [sp, -32]!  \n"
-".seh_save_fplr_x 32  \n"
-"  mov fp, sp  \n"
-".seh_set_fp  \n"
-".seh_endprologue  \n"
+"\n .seh_proc do_call_once_seh_take_over"
+"\n .seh_handler do_arm64_call_once_on_except, @except"
+"\n   stp fp, lr, [sp, -32]!"
+"\n .seh_save_fplr_x 32"
+"\n   mov fp, sp"
+"\n .seh_set_fp"
+"\n .seh_endprologue"
 /* Stash `once` for the handler.  */
-"  str x0, [sp, 16]  \n"
+"\n   str x0, [sp, 16]"
 /* Make the call `(*init_proc) (arg)`.  */
-"  mov x0, x2  \n"
+"\n   mov x0, x2"
 #  if defined __arm64ec__
-"  mov x11, x1  \n"
-"  bl __MCF_arm64ec_icall_helper_p  \n"
+"\n   mov x11, x1"
+"\n   bl __MCF_arm64ec_icall_helper_p"
 #  else
-"  blr x1  \n"
+"\n   blr x1"
 #  endif
 /* Disarm the once flag with a tail call.  */
-"  ldr x0, [sp, 16]  \n"
-".seh_startepilogue  \n"
-"  ldp fp, lr, [sp], 32  \n"
-".seh_save_fplr_x 32  \n"
-".seh_endepilogue  \n"
-"  b _MCF_once_release  \n"
-".seh_endproc  \n"
+"\n   ldr x0, [sp, 16]"
+"\n .seh_startepilogue"
+"\n   ldp fp, lr, [sp], 32"
+"\n .seh_save_fplr_x 32"
+"\n .seh_endepilogue"
+"\n   b _MCF_once_release"
+"\n .seh_endproc"
 /* Define the exception handler, which is called either when an exception is
  * raised, or the stack is being unwound.  */
-".def do_arm64_call_once_on_except; .scl 3; .type 32; .endef  \n"
-"do_arm64_call_once_on_except:  \n"
-"  stp fp, lr, [sp, -16]!  \n"
+"\n .def do_arm64_call_once_on_except; .scl 3; .type 32; .endef"
+"\n do_arm64_call_once_on_except:"
+"\n   stp fp, lr, [sp, -16]!"
 /* Check whether `ExceptionFlags` contains `EXCEPTION_UNWINDING`.  */
-"  ldr w8, [x0, 4]  \n"
-"  tbz w8, 1, 3001f  \n"
+"\n   ldr w8, [x0, 4]"
+"\n   tbz w8, 1, 3001f"
 /* Locate the once flag from `EstablisherFrame`, and reset it.  */
-"  ldur x0, [x1, -16]  \n"
-"  bl _MCF_once_abort  \n"
-"3001:  \n"
+"\n   ldur x0, [x1, -16]"
+"\n   bl _MCF_once_abort"
+"\n 3001:"
 /* Return `ExceptionContinueSearch`.  */
-"  mov w0, 1  \n"
-"  ldp fp, lr, [sp], 16  \n"
-"  ret  \n"
+"\n   mov w0, 1"
+"\n   ldp fp, lr, [sp], 16"
+"\n   ret"
 #endif
-".text  \n"
+"\n .text"
 );
 
 __MCF_DLLEXPORT __MCF_FN_PURE
