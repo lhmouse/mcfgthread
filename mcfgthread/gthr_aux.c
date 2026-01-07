b6160890b861107513a893c1c2879b78bd184cc2
LIU Hao <lh_mouse@126.com>
2026-01-07 11:01:32 +0800

    gthr_aux: Copy GUID with `__builtin_memcpy()`
    With SSE2 or NEON it's only two instructions.
    
    (cherry picked from commit efd7ae34b82c74048df00133c1bba5b543f4b39f)
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mcfgthread/gthr_aux.c
+++ mcfgthread/gthr_aux.c
@@ -365,7 +365,7 @@ _MCF_thread*
 __MCF_gthr_thread_create_v3(__MCF_gthr_thread_procedure* proc, void* arg)
   {
     __MCF_gthr_thread_record record;
-    __MCF_mcopy(record.__magic_guid, __MCF_crt_gthread_guid, 16);
+    __builtin_memcpy(record.__magic_guid, __MCF_crt_gthread_guid, 16);
     record.__proc = proc;
     record.__arg_or_result = arg;
     return _MCF_thread_new(do_gthr_thread_thunk_v3, &record, sizeof(record));
