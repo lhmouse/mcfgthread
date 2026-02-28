8f730d6496c102ab02f048ff163f90b7d88adfbd
LIU Hao <lh_mouse@126.com>
2026-02-28 17:34:52 +0800

    test/at{exit,_quick_exit}_terminate: New tests

---
+++ test/at_quick_exit_terminate.cpp
@@ -0,0 +1,39 @@
+/* This file is part of MCF Gthread.
+ * Copyright (C) 2022-2026 LH_Mouse. All wrongs reserved.
+ *
+ * MCF Gthread is free software. Licensing information is included in
+ * LICENSE.TXT as a whole. The GCC Runtime Library Exception applies
+ * to this file.  */
+
+#include "../mcfgthread/cxa.h"
+#include "../mcfgthread/exit.h"
+#include <assert.h>
+#include <stdio.h>
+#include <exception>
+
+int
+main(void)
+  {
+    ::std::set_terminate(
+      +[] {
+        ::fprintf(stderr, "terminating\n");
+        ::__MCF__Exit(0);
+      });
+
+    ::__MCF_cxa_at_quick_exit(
+      +[](void* ptr) {
+        ::fprintf(stderr, "throwing %p\n", ptr);
+        throw ptr;
+      },
+      (void*) 42,
+      nullptr);
+
+    try {
+      ::__MCF_quick_exit(1);
+      ::fprintf(stderr, "unreachable\n");
+    }
+    catch(void* ptr) {
+      ::fprintf(stderr, "caught %p\n", ptr);
+      return 1;
+    }
+  }
