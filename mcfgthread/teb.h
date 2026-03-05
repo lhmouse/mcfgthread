d97506691f3f098bd17f7dac685680f3b9c29e39
LIU Hao <lh_mouse@126.com>
2026-03-05 11:23:20 +0800

    teb: Check for ISAs first

--- mcfgthread/teb.h
+++ mcfgthread/teb.h
@@ -24,18 +24,22 @@ void*
 __MCF_teb(void)
   __MCF_noexcept
   {
-#if defined __clang__ && defined __MCF_M_X8664_ASM
+#if defined __MCF_M_X8664_ASM
+#  if defined __clang__
     return *(void* __seg_gs*) 0x30;
-#elif defined __clang__ && defined __MCF_M_X8632_ASM
-    return *(void* __seg_fs*) 0x18;
-#elif defined __MCF_M_X8664_ASM
+#  else
     void* __teb;
     __asm__ ("mov { %%gs:0x30, %0 | %0, gs:[0x30] }" : "=r"(__teb));
     return __teb;
+#  endif
 #elif defined __MCF_M_X8632_ASM
+#  if defined __clang__
+    return *(void* __seg_fs*) 0x18;
+#  else
     void* __teb;
     __asm__ ("mov { %%fs:0x18, %0 | %0, fs:[0x18] }" : "=r"(__teb));
     return __teb;
+#  endif
 #elif defined __MCF_M_ARM64_ASM
     register void* __teb __asm__("x18");
     __asm__ ("" : "=r"(__teb));
@@ -56,15 +60,15 @@ uint32_t
 __MCF_teb_load_32(uint32_t __offset)
   __MCF_noexcept
   {
-#if defined __clang__ && defined __MCF_M_X8664_ASM
+#if defined __MCF_M_X8664_ASM && defined __clang__
     return *(uint32_t __seg_gs*) __offset;
-#elif defined __clang__ && defined __MCF_M_X8632_ASM
+#elif defined __MCF_M_X8632_ASM && defined __clang__
     return *(uint32_t __seg_fs*) __offset;
-#elif defined _MSC_VER && !defined __clang__ && defined __MCF_M_X8664
+#elif defined __MCF_M_X8664 && defined _MSC_VER && !defined __clang__
     return __readgsdword(__offset);
-#elif defined _MSC_VER && !defined __clang__ && defined __MCF_M_X8632
+#elif defined __MCF_M_X8632 && defined _MSC_VER && !defined __clang__
     return __readfsdword(__offset);
-#elif defined _MSC_VER && !defined __clang__ && defined __MCF_M_ARM64
+#elif defined __MCF_M_ARM64 && defined _MSC_VER && !defined __clang__
     return __readx18dword(__offset);
 #else
     return *((uint32_t*) __MCF_teb() + __offset / 4);
@@ -76,15 +80,15 @@ void
 __MCF_teb_store_32(uint32_t __offset, uint32_t __value)
   __MCF_noexcept
   {
-#if defined __clang__ && defined __MCF_M_X8664_ASM
+#if defined __MCF_M_X8664_ASM && defined __clang__
     *(uint32_t __seg_gs*) __offset = __value;
-#elif defined __clang__ && defined __MCF_M_X8632_ASM
+#elif defined __MCF_M_X8632_ASM && defined __clang__
     *(uint32_t __seg_fs*) __offset = __value;
-#elif defined _MSC_VER && !defined __clang__ && defined __MCF_M_X8664
+#elif defined __MCF_M_X8664 && defined _MSC_VER && !defined __clang__
     __writegsdword(__offset, __value);
-#elif defined _MSC_VER && !defined __clang__ && defined __MCF_M_X8632
+#elif defined __MCF_M_X8632 && defined _MSC_VER && !defined __clang__
     __writefsdword(__offset, __value);
-#elif defined _MSC_VER && !defined __clang__ && defined __MCF_M_ARM64
+#elif defined __MCF_M_ARM64 && defined _MSC_VER && !defined __clang__
     __writex18dword(__offset, __value);
 #else
     *((uint32_t*) __MCF_teb() + __offset / 4) = __value;
@@ -96,15 +100,15 @@ void*
 __MCF_teb_load_ptr(uint32_t __offset)
   __MCF_noexcept
   {
-#if defined __clang__ && defined __MCF_M_X8664_ASM
+#if defined __MCF_M_X8664_ASM && defined __clang__
     return *(void* __seg_gs*) __offset;
-#elif defined __clang__ && defined __MCF_M_X8632_ASM
+#elif defined __MCF_M_X8632_ASM && defined __clang__
     return *(void* __seg_fs*) __offset;
-#elif defined _MSC_VER && !defined __clang__ && defined __MCF_M_X8664
+#elif defined __MCF_M_X8664 && defined _MSC_VER && !defined __clang__
     return (void*) __readgsqword(__offset);
-#elif defined _MSC_VER && !defined __clang__ && defined __MCF_M_X8632
+#elif defined __MCF_M_X8632 && defined _MSC_VER && !defined __clang__
     return (void*) __readfsdword(__offset);
-#elif defined _MSC_VER && !defined __clang__ && defined __MCF_M_ARM64
+#elif defined __MCF_M_ARM64 && defined _MSC_VER && !defined __clang__
     return (void*) __readx18qword(__offset);
 #else
     return *((void**) __MCF_teb() + __offset / __MCF_64_32(8, 4));
@@ -116,15 +120,15 @@ void
 __MCF_teb_store_ptr(uint32_t __offset, const void* __value)
   __MCF_noexcept
   {
-#if defined __clang__ && defined __MCF_M_X8664_ASM
+#if defined __MCF_M_X8664_ASM && defined __clang__
     *(const void* __seg_gs*) __offset = __value;
-#elif defined __clang__ && defined __MCF_M_X8632_ASM
+#elif defined __MCF_M_X8632_ASM && defined __clang__
     *(const void* __seg_fs*) __offset = __value;
-#elif defined _MSC_VER && !defined __clang__ && defined __MCF_M_X8664
+#elif defined __MCF_M_X8664 && defined _MSC_VER && !defined __clang__
     __writegsqword(__offset, (uint64_t) __value);
-#elif defined _MSC_VER && !defined __clang__ && defined __MCF_M_X8632
+#elif defined __MCF_M_X8632 && defined _MSC_VER && !defined __clang__
     __writefsdword(__offset, (uint32_t) __value);
-#elif defined _MSC_VER && !defined __clang__ && defined __MCF_M_ARM64
+#elif defined __MCF_M_ARM64 && defined _MSC_VER && !defined __clang__
     __writex18qword(__offset, (uint64_t) __value);
 #else
     *((const void**) __MCF_teb() + __offset / __MCF_64_32(8, 4)) = __value;
