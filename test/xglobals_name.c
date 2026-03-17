a5c584bc73763ca53be68f9732c4d4581a81a228
LIU Hao <lh_mouse@126.com>
2026-03-17 15:28:20 +0800

    test/xglobals_name: New test

---
+++ test/xglobals_name.c
@@ -0,0 +1,62 @@
+/* This file is part of MCF Gthread.
+ * Copyright (C) 2022-2026 LH_Mouse. All wrongs reserved.
+ *
+ * MCF Gthread is free software. Licensing information is included in
+ * LICENSE.TXT as a whole. The GCC Runtime Library Exception applies
+ * to this file.  */
+
+#define WIN32_LEAN_AND_MEAN  1
+#include <windows.h>
+#include <winternl.h>
+#include <ntstatus.h>
+#include <stdio.h>
+#include <assert.h>
+
+NTSYSAPI
+NTSTATUS
+NTAPI
+BaseGetNamedObjectDirectory(
+  OUT HANDLE* NamedObjectDirectory);
+
+NTSYSAPI
+NTSTATUS
+NTAPI
+NtOpenSection(
+  OUT HANDLE* SectionHandle,
+  IN ACCESS_MASK DesiredAccess,
+  IN OBJECT_ATTRIBUTES* ObjectAttributes);
+
+extern void* __MCF_g;
+
+int
+main(void)
+  {
+    WCHAR gname_str[128] = L"Local\\__MCF_crt_xglobals_";
+    WCHAR* eptr = gname_str + wcslen(gname_str);
+    UINT32 pid = GetCurrentProcessId();
+    for(UINT i = 0; i != 8; ++i)
+      *(eptr ++) = L"0123456789ABCDEF"[(pid >> (28 - i * 4)) & 0x0FU];
+    *(eptr ++) = '_';
+    UINT64 cookie = (UINT64) pid << 32 | pid;
+    cookie = (UINT_PTR) EncodePointer((PVOID)(UINT_PTR) ~cookie) * 0x9E3779B97F4A7C15ULL;
+    for(UINT i = 0; i != 16; ++i)
+      *(eptr ++) = L"GHJKLMNPQRSTUWXY"[(cookie >> (60 - i * 4)) & 0x0FU];
+    *eptr = 0;
+    fprintf(stderr, "gname_str = %ls\n", gname_str);
+
+    UNICODE_STRING gname = { .Length = (USHORT) ((eptr - gname_str) * 2),
+                             .MaximumLength = sizeof(gname_str),
+                             .Buffer = gname_str };
+    OBJECT_ATTRIBUTES attrs = { .Length = sizeof(OBJECT_ATTRIBUTES),
+                                .ObjectName = &gname };
+    assert(BaseGetNamedObjectDirectory(&attrs.RootDirectory) == STATUS_SUCCESS);
+
+    HANDLE hmap;
+    assert(NtOpenSection(&hmap, SECTION_ALL_ACCESS, &attrs) == STATUS_ACCESS_DENIED);
+    attrs.Attributes = OBJ_EXCLUSIVE;
+    assert(NtOpenSection(&hmap, SECTION_ALL_ACCESS, &attrs) == STATUS_SUCCESS);
+
+    char* gmem = MapViewOfFile(hmap, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+    assert(gmem);
+    assert(*(void**) gmem == __MCF_g);
+  }
