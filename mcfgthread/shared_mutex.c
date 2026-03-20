85fb7fa5594850293eb356ef37d26608c28db238
LIU Hao <lh_mouse@126.com>
2026-03-21 02:33:26 +0800

    shared_mutex: Backport 69964745636e95ca23bae5e22786c0191918d776 from master
    (cherry picked from commit ee71808b1e23e067215f49ee962e87e550b25fe6)
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- mcfgthread/shared_mutex.c
+++ mcfgthread/shared_mutex.c
@@ -137,29 +137,30 @@ _MCF_shared_mutex_lock_exclusive_slow(_MCF_shared_mutex* smutex, const int64_t* 
 
 __MCF_DLLEXPORT __MCF_NEVER_INLINE
 void
-_MCF_shared_mutex_unlock_slow(_MCF_shared_mutex* smutex)
+_MCF_shared_mutex_unlock_slow(_MCF_shared_mutex* smtx)
   {
+    size_t wake_num;
+    _MCF_shared_mutex old, new;
+
     /* Determine whether the shared mutex has been locked in shared mode
      * (`__nshare` <= `__MCF_SHARED_MUTEX_MAX_SHARE`) or exclusive mode. The
      * last reader (`__nshare` = 1 before the call) or writer (`__nshare` =
-     * `__MCF_SHARED_MUTEX_NSHARE_M` before the call) that unlocks a shared
-     * mutex shall perform a wakeup operation.  */
-    bool wake_one;
-    _MCF_shared_mutex old, new;
-    _MCF_atomic_load_pptr_rlx(&old, smutex);
-    do {
-      bool was_exclusive = old.__nshare == 0x3FFF;
-      bool was_shared = (old.__nshare != 0) && !was_exclusive;
-      __MCF_ASSERT(was_exclusive || was_shared);
-      wake_one = (old.__nsleep != 0) && ((old.__nshare == 1) || was_exclusive);
+     * `0x3FFF` before the call) that unlocks a shared mutex shall perform a
+     * wakeup operation. Since we can't know whether waiters are waiting for
+     * shared or exclusive access, we have to wake up all threads.  */
+    _MCF_atomic_load_pptr_rlx(&old, smtx);
+    for(;;) {
+      __MCF_ASSERT(old.__nshare != 0);
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wconversion"
-      new.__nshare = old.__nshare - was_shared + was_exclusive;
-      new.__nsleep = old.__nsleep - wake_one;
-#pragma GCC diagnostic pop
+      uintptr_t new_nshare = (old.__nshare - 1U) & (((uint32_t) old.__nshare - 0x3FFFU) >> 14);
+      wake_num = old.__nsleep & ((new_nshare - 1U) >> 14);
+      new.__nshare = new_nshare & 0x3FFFU;
+      new.__nsleep = (old.__nsleep - wake_num) & (__MCF_UPTR_MAX >> 14);
+
+      if(_MCF_atomic_cmpxchg_weak_pptr_rel(smtx, &old, &new))
+        break;
     }
-    while(!_MCF_atomic_cmpxchg_weak_pptr_arl(smutex, &old, &new));
 
-    __MCF_batch_release_common(smutex, wake_one);
+    /* Wake up these threads.  */
+    __MCF_batch_release_common(smtx, wake_num);
   }
