ae236f6e7baa6f82f798abd1c920948190dd95f3
LIU Hao <lh_mouse@126.com>
2026-03-22 00:15:51 +0800

    test/shared_mutex_rw_multi: Use multiple writers
    (cherry picked from commit 6f9bab8a2dc7dba7e593288690390e039673d2c3)
    Signed-off-by: LIU Hao <lh_mouse@126.com>

--- test/shared_mutex_rw_multi.c
+++ test/shared_mutex_rw_multi.c
@@ -11,12 +11,13 @@
 #include <assert.h>
 #include <stdio.h>
 
-#define NTHREADS  64U
-static _MCF_thread* threads[NTHREADS];
+#define NTHREADS  32U
+static _MCF_thread* reader_threads[NTHREADS];
+static _MCF_thread* writer_threads[NTHREADS];
 static _MCF_shared_mutex mutex;
 static _MCF_sem start = __MCF_SEM_INIT(0);
 
-#define NITER  100U
+#define NITER  100000U
 static int res1 = 0, res2 = 0;
 
 static
@@ -31,49 +32,60 @@ reader_thread_proc(_MCF_thread* self)
 
       /* Check resources are equal.  */
       int t1 = res1;
-      _MCF_sleep((const int64_t[]) { -1 });
+      _MCF_yield();
       assert(t1 == res2);
       _MCF_shared_mutex_unlock(&mutex);
 
       //fprintf(stderr, "R    thread %d\n", __MCF_tid());
-      _MCF_sleep((const int64_t[]) { -5 });
+      _MCF_yield();
     }
 
-    fprintf(stderr, "thread %d quitting\n", self->__tid);
+    fprintf(stderr, "R    thread %d quitting\n", self->__tid);
   }
 
-int
-main(void)
+static
+void
+writer_thread_proc(_MCF_thread* self)
   {
-    for(size_t k = 0;  k < NTHREADS;  ++k) {
-      threads[k] = _MCF_thread_new(reader_thread_proc, __MCF_nullptr, 0);
-      assert(threads[k]);
-    }
+    _MCF_sem_wait(&start, __MCF_nullptr);
 
-    fprintf(stderr, "main waiting\n");
-    _MCF_sem_signal_some(&start, NTHREADS);
-
-    // main thread is the writer.
     for(uint32_t i = 0; i < NITER;  ++i) {
       int r = _MCF_shared_mutex_lock_exclusive(&mutex, __MCF_nullptr);
       assert(r == 0);
 
       /* Add resources.  */
       res1 ++;
-      _MCF_sleep((const int64_t[]) { -1 });
+      _MCF_yield();
       res2 ++;
       assert(res1 == res2);
       _MCF_shared_mutex_unlock(&mutex);
 
       //fprintf(stderr, "  W  thread %d\n", __MCF_tid());
-      _MCF_sleep((const int64_t[]) { -5 });
+      _MCF_yield();
     }
 
+    fprintf(stderr, "  W  thread %d quitting\n", self->__tid);
+  }
+
+int
+main(void)
+  {
     for(size_t k = 0;  k < NTHREADS;  ++k) {
-      _MCF_thread_wait(threads[k], __MCF_nullptr);
+      reader_threads[k] = _MCF_thread_new(reader_thread_proc, __MCF_nullptr, 0);
+      assert(reader_threads[k]);
+      writer_threads[k] = _MCF_thread_new(writer_thread_proc, __MCF_nullptr, 0);
+      assert(writer_threads[k]);
+    }
+
+    fprintf(stderr, "main waiting\n");
+    _MCF_sem_signal_some(&start, NTHREADS * 2);
+
+    for(size_t k = 0;  k < NTHREADS;  ++k) {
+      _MCF_thread_wait(reader_threads[k], __MCF_nullptr);
+      _MCF_thread_wait(writer_threads[k], __MCF_nullptr);
       fprintf(stderr, "main wait finished: %d\n", (int)k);
     }
 
-    assert(res1 == NITER);
-    assert(res2 == NITER);
+    assert(res1 == NITER * NTHREADS);
+    assert(res2 == NITER * NTHREADS);
   }
