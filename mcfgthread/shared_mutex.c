0d93743194c55172c935c4db546c9988eb64adfb
LIU Hao <lh_mouse@126.com>
2026-03-22 01:24:34 +0800

    shared_mutex: Unify slow path functions

--- mcfgthread/shared_mutex.c
+++ mcfgthread/shared_mutex.c
@@ -11,9 +11,9 @@
 #include "shared_mutex.h"
 #include "xglobals.h"
 
-__MCF_DLLEXPORT
+static
 int
-_MCF_shared_mutex_lock_shared_slow(_MCF_shared_mutex* smtx, const int64_t* timeout_opt)
+do_lock_common(_MCF_shared_mutex* smtx, bool shared, const int64_t* timeout_opt)
   {
     __MCF_winnt_timeout nt_timeout = __MCF_0_INIT;
     _MCF_shared_mutex old, new;
@@ -26,7 +26,7 @@ _MCF_shared_mutex_lock_shared_slow(_MCF_shared_mutex* smtx, const int64_t* timeo
   try_lock_loop:
     _MCF_atomic_load_pptr_rlx(&old, smtx);
     for(;;)
-      if((old.__nshare < __MCF_SHARED_MUTEX_MAX_SHARE) && (old.__nsleep == 0)) {
+      if(shared && (old.__nshare < __MCF_SHARED_MUTEX_MAX_SHARE) && (old.__nsleep == 0)) {
         /* The mutex is either not locked or locked in shared mode, and no
          * thread is requesting exclusive access, so grant shared access.  */
         new.__nshare = (old.__nshare + 1U) & 0x3FFFU;
@@ -35,9 +35,16 @@ _MCF_shared_mutex_lock_shared_slow(_MCF_shared_mutex* smtx, const int64_t* timeo
         if(_MCF_atomic_cmpxchg_weak_pptr_acq(smtx, &old, &new))
           return 0;
       }
+      else if(!shared && old.__nshare == 0) {
+        /* The mutex is not locked, so grant exclusive access.  */
+        new.__nshare = 0x3FFFU;
+        new.__nsleep = old.__nsleep;
+
+        if(_MCF_atomic_cmpxchg_weak_pptr_acq(smtx, &old, &new))
+          return 0;
+      }
       else if(nt_timeout.__li.QuadPart == 0) {
-        /* Shared access can't be granted, and we are not willing to wait, so
-         * fail.  */
+        /* Access can't be granted, and we are not willing to wait, so fail.  */
         return -1;
       }
       else {
@@ -84,72 +91,16 @@ _MCF_shared_mutex_lock_shared_slow(_MCF_shared_mutex* smtx, const int64_t* timeo
 
 __MCF_DLLEXPORT
 int
+_MCF_shared_mutex_lock_shared_slow(_MCF_shared_mutex* smtx, const int64_t* timeout_opt)
+  {
+    return do_lock_common(smtx, true, timeout_opt);
+  }
+
+__MCF_DLLEXPORT
+int
 _MCF_shared_mutex_lock_exclusive_slow(_MCF_shared_mutex* smtx, const int64_t* timeout_opt)
   {
-    __MCF_winnt_timeout nt_timeout = __MCF_0_INIT;
-    _MCF_shared_mutex old, new;
-
-    /* Initialize the timeout value if a non-zero duration is specified. A
-     * null pointer denotes infinity.  */
-    if(!timeout_opt || (*timeout_opt != 0))
-      __MCF_initialize_winnt_timeout_v3(&nt_timeout, timeout_opt);
-
-  try_lock_loop:
-    _MCF_atomic_load_pptr_rlx(&old, smtx);
-    for(;;)
-      if(old.__nshare == 0) {
-        /* The mutex is not locked, so grant exclusive access.  */
-        new.__nshare = 0x3FFFU;
-        new.__nsleep = old.__nsleep;
-
-        if(_MCF_atomic_cmpxchg_weak_pptr_acq(smtx, &old, &new))
-          return 0;
-      }
-      else if(nt_timeout.__li.QuadPart == 0) {
-        /* Exclusive access can't be granted, and we are not willing to wait,
-         * so fail.  */
-        return -1;
-      }
-      else {
-        /* Allocate a sleeping count for the current thread.  */
-        new.__nshare = old.__nshare;
-        new.__nsleep = (old.__nsleep + 1U) & (__MCF_UPTR_MAX >> 14);
-
-        if(_MCF_atomic_cmpxchg_weak_pptr_rlx(smtx, &old, &new))
-          break;
-      }
-
-    /* Try waiting.  */
-    int err = __MCF_keyed_event_wait(smtx, &nt_timeout);
-    while(err != 0) {
-      /* Tell another thread which is going to signal this mutex that an old
-       * waiter has left by decrementing the number of sleeping threads. But
-       * see below...  */
-      _MCF_atomic_load_pptr_rlx(&old, smtx);
-      for(;;) {
-        if(old.__nsleep == 0)
-          break;
-
-        new.__nshare = old.__nshare;
-        new.__nsleep = (old.__nsleep - 1U) & (__MCF_UPTR_MAX >> 14);
-
-        if(_MCF_atomic_cmpxchg_weak_pptr_rlx(smtx, &old, &new))
-          return -1;
-      }
-
-      /* ... It is possible that a second thread has already decremented the
-       * counter. If this does take place, it is going to release the keyed
-       * event soon. We must still wait, otherwise we get a deadlock in the
-       * second thread. However, a third thread could start waiting for this
-       * keyed event before us, so we set the timeout to zero. If we time out
-       * once more, the third thread will have incremented the number of
-       * sleeping threads and we can try decrementing it again.  */
-      err = __MCF_keyed_event_wait(smtx, &(__MCF_winnt_timeout) { 0 });
-    }
-
-    /* We have got notified.  */
-    __MCF_adjust_winnt_timeout_v3(&nt_timeout);
-    goto try_lock_loop;
+    return do_lock_common(smtx, false, timeout_opt);
   }
 
 __MCF_DLLEXPORT __MCF_NEVER_INLINE
