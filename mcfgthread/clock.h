0f83eca2c498026a50a47c3eff2e27bae3f03926
LIU Hao <lh_mouse@126.com>
2026-07-16 17:16:17 +0800

    clock: Use JavaDoc-style comments

--- mcfgthread/clock.h
+++ mcfgthread/clock.h
@@ -16,46 +16,85 @@ __MCF_CXX(extern "C" {)
 #  define __MCF_CLOCK_INLINE  __MCF_GNU_INLINE
 #endif
 
-/* Get the number of milliseconds since 1970-01-01T00:00:00Z. This value
- * corresponds to `clock_gettime(CLOCK_REALTIME, ...)` on Linux.  */
+/* Gets the system time.
+ *
+ * This function is fast, but may be less precise than `_MCF_hires_utc_now()`.
+ * This value corresponds to `clock_gettime(CLOCK_REALTIME_COARSE, ...)` on
+ * Linux.
+ *
+ * @returns the number of milliseconds since 1970-01-01T00:00:00Z.  */
 __MCF_CLOCK_IMPORT
 int64_t
 _MCF_utc_now(void)
   __MCF_noexcept;
 
+/* Gets a more precise value of the system time.
+ *
+ * This function is precise, but may be slower than `_MCF_utc_now()`. This
+ * value corresponds to `clock_gettime(CLOCK_REALTIME, ...)` on Linux.
+ *
+ * @returns the number of milliseconds since 1970-01-01T00:00:00Z.  */
 __MCF_CLOCK_IMPORT
 double
 _MCF_hires_utc_now(void)
   __MCF_noexcept;
 
-/* Get the number of milliseconds since system startup, excluding time when the
- * system is in suspension or hibernation. This value corresponds to
- * `clock_gettime(CLOCK_MONOTONIC, ...)` on Linux.  */
+/* Gets the number of milliseconds since system startup, excluding time when the
+ * system is in suspension or hibernation.
+ *
+ * This function is fast, but may be less precise than `_MCF_hires_steady_now()`.
+ * This value corresponds to `clock_gettime(CLOCK_MONOTONIC_COARSE, ...)` on
+ * Linux.
+ *
+ * @returns the number of milliseconds of the steady clock.  */
 __MCF_CLOCK_IMPORT
 int64_t
 _MCF_steady_now(void)
   __MCF_noexcept;
 
+/* Gets the number of milliseconds since system startup, excluding time when the
+ * system is in suspension or hibernation.
+ *
+ * This function is precise, but may be slower than `_MCF_steady_now()`. This
+ * value corresponds to `clock_gettime(CLOCK_MONOTONIC, ...)` on Linux.
+ *
+ * @returns the number of milliseconds of the steady clock.  */
 __MCF_CLOCK_IMPORT
 double
 _MCF_hires_steady_now(void)
   __MCF_noexcept;
 
-/* Get the number of milliseconds since system startup. This value corresponds
- * to `clock_gettime(CLOCK_BOOTTIME, ...)` on Linux.  */
+/* Gets the number of milliseconds since system startup, including time when the
+ * system is in suspension or hibernation.
+ *
+ * This function is fast, but may be less precise than `_MCF_hires_tick_count()`.
+ * This value corresponds to `clock_gettime(CLOCK_BOOTTIME, ...)` on Linux.
+ *
+ * @returns the number of milliseconds of the steady clock.  */
 __MCF_CLOCK_IMPORT
 int64_t
 _MCF_tick_count(void)
   __MCF_noexcept;
 
+/* Gets the number of milliseconds since system startup, including time when the
+ * system is in suspension or hibernation.
+ *
+ * This function is precise, but may be slower than `_MCF_tick_count()`. This
+ * value corresponds to `clock_gettime(CLOCK_BOOTTIME, ...)` on Linux.
+ *
+ * @returns the number of milliseconds of the steady clock.  */
 __MCF_CLOCK_IMPORT
 double
 _MCF_hires_tick_count(void)
   __MCF_noexcept;
 
-/* Gets the value of the performance counter on the current platform. This is
- * the traditional high-resolution clock for measurement, but it may deviate
- * from `_MCF_hires_tick_count()` by a few milliseconds.  */
+/* Gets the value of the high-resolution performance counter as a number of
+ * milliseconds.
+ *
+ * This is the traditional high-resolution clock for measurement, but it may
+ * deviate from `_MCF_hires_tick_count()` by a few milliseconds.
+ *
+ * @returns the number of milliseconds of the performance counter.  */
 __MCF_CLOCK_IMPORT
 double
 _MCF_perf_counter(void)
