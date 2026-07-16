d6ef07b4766af58ce64ef55cab8054bb50c19ce7
LIU Hao <lh_mouse@126.com>
2026-07-16 23:07:16 +0800

    once: Update comments

--- mcfgthread/once.h
+++ mcfgthread/once.h
@@ -17,7 +17,8 @@ __MCF_CXX(extern "C" {)
 #  define __MCF_ONCE_INLINE  __MCF_GNU_INLINE
 #endif
 
-/* Define the once flag struct, which takes up the same storage as a pointer.  */
+/* This is the once-initialization flag, which takes up the same storage as a
+ * pointer.  */
 struct __MCF_once
   {
     __MCF_EX uintptr_t __ready : 8;  /* this conforms to the Itanium C++ ABI  */
@@ -25,12 +26,15 @@ struct __MCF_once
     __MCF_EX uintptr_t __nsleep : __MCF_PTR_BITS - 9;  /* number of sleeping threads  */
   };
 
-/* Initializes a once-initialization flag dynamically. Static ones should be
- * initialized with `{0}`, like other structs.
+/* Initializes a once-initialization flag dynamically.
  *
- * Each once-initialization flag can be in any of the three states: UNLOCKED,
- * LOCKED and READY. An once-initialization flag is initialized to the UNLOCKED
- * state.  */
+ * Static ones should be initialized with `{0}`, like other structs.
+ *
+ * A once-initialization flag can be in any of the three states: UNLOCKED,
+ * LOCKED and READY. An once-initialization flag is initialized to UNLOCKED.
+ *
+ * @param `once` points to the once-initialization flag to initialize.
+ * @returns nothing.  */
 __MCF_ONCE_INLINE
 void
 _MCF_once_init(_MCF_once* __once)
@@ -38,54 +42,91 @@ _MCF_once_init(_MCF_once* __once)
 
 /* Attempts to lock a once-initialization flag.
  *
- * The return value of this function has the same semantics with the
- * `__cxa_guard_acquire()` function from the Itanium C++ ABI, as explained on
- * <https://itanium-cxx-abi.github.io/cxx-abi/abi.html#once-ctor>.
+ * If this once-initialization flag is in the UNLOCKED state, this function
+ * changes it into the LOCKED state and returns 1. If it is in the LOCKED state
+ * because another thread has locked it, this function blocks until the other
+ * thread releases the lock, or returns -1 if the operation has timed out. If
+ * the once flag is already in the READY state, this function does nothing and
+ * returns 0 immediately.
  *
- * If `__timeout_opt` points to a positive integer, it denotes the expiration
- * time in number of milliseconds since 1970-01-01T00:00:00Z. If it points to a
- * negative integer, the absolute value of it denotes the number of milliseconds
- * to wait. If it points to zero, the function returns immediately without
- * waiting. If it is null, the function waits indefinitely.
+ * The return value of this function has the same semantics with the
+ * `__cxa_guard_acquire()` function from the Itanium C++ ABI.
+ *
+ * @param `once` points to the once-initialization flag to lock.
+ * @param `timeout_opt` points to the timeout value. If it is positive, it
+ *     denotes the expiration time point in the number of milliseconds since
+ *     1970-01-01T00:00:00Z. If it points to a negative integer, the absolute
+ *     value of it denotes the number of milliseconds to wait. If it points to
+ *     zero, the function returns immediately without waiting. If it is null,
+ *     the function waits indefinitely.
+ * @returns 1 if the once-initialization flag has been successfully locked and
+ *     the caller should perform initialization, 0 if initialization has finished
+ *     and the caller should do nothing, or -1 if the operation has timed out.  */
+__MCF_ONCE_IMPORT
+int
+_MCF_once_wait_slow(_MCF_once* __once, const int64_t* __timeout_opt)
+  __MCF_noexcept;
+
+/* Attempts to lock a once-initialization flag.
  *
  * If this once-initialization flag is in the UNLOCKED state, this function
  * changes it into the LOCKED state and returns 1. If it is in the LOCKED state
  * because another thread has locked it, this function blocks until the other
  * thread releases the lock, or returns -1 if the operation has timed out. If
  * the once flag is already in the READY state, this function does nothing and
- * returns 0 immediately.  */
-__MCF_ONCE_IMPORT
-int
-_MCF_once_wait_slow(_MCF_once* __once, const int64_t* __timeout_opt)
-  __MCF_noexcept;
-
+ * returns 0 immediately.
+ *
+ * The return value of this function has the same semantics with the
+ * `__cxa_guard_acquire()` function from the Itanium C++ ABI.
+ *
+ * This is an inline wrapper for `_MCF_once_wait_slow()`, which does nothing
+ * if the once-initialization flag is already in the READY state.
+ *
+ * @param `once` points to the once-initialization flag to lock.
+ * @param `timeout_opt` points to the timeout value. If it is positive, it
+ *     denotes the expiration time point in the number of milliseconds since
+ *     1970-01-01T00:00:00Z. If it points to a negative integer, the absolute
+ *     value of it denotes the number of milliseconds to wait. If it points to
+ *     zero, the function returns immediately without waiting. If it is null,
+ *     the function waits indefinitely.
+ * @returns 1 if the once-initialization flag has been successfully locked and
+ *     the caller should perform initialization, 0 if initialization has finished
+ *     and the caller should do nothing, or -1 if the operation has timed out.  */
 __MCF_ONCE_INLINE
 int
 _MCF_once_wait(_MCF_once* __once, const int64_t* __timeout_opt)
   __MCF_noexcept;
 
-/* Cancels a once-initialization flag which shall be in the LOCKED state. If
- * the flag has not been locked already, the behavior is undefined.
+/* Cancels once initialization.
  *
- * This function changes it into the UNLOCKED state and wakes up the next
- * thread that is sleeping on this flag, whose call to `_MCF_once_wait()` will
- * return 1.  */
+ * This function changes the once-initialization flag to the UNLOCKED state and
+ * wakes up the next thread that is waiting for it, which will observe
+ * `_MCF_once_wait()` return a value of 1. The caller shall have locked the
+ * once-initialization flag, otherwise the behavior is undefined.
+ *
+ * @param `once` points to the once-initialization flag to cancel.
+ * @returns nothing.  */
 __MCF_ONCE_IMPORT
 void
 _MCF_once_abort(_MCF_once* __once)
   __MCF_noexcept;
 
-/* Releases a once-initialization flag which shall be in the LOCKED state. If
- * the flag has not been locked already, the behavior is undefined.
+/* Completes once initialization.
  *
- * This function changes it into the READY state and wakes up all threads that
- * are sleeping on this flag, whose calls to `_MCF_once_wait()` will return 0.  */
+ * This function changes the once-initialization flag to the READY state and
+ * wakes up all threads that are waiting for it, which will observe
+ * `_MCF_once_wait()` return a value of 0. The caller shall have locked the
+ * once-initialization flag, otherwise the behavior is undefined.
+ *
+ * @param `once` points to the once-initialization flag to complete.
+ * @returns nothing.  */
 __MCF_ONCE_IMPORT
 void
 _MCF_once_release(_MCF_once* __once)
   __MCF_noexcept;
 
 /* Define inline functions after all declarations.
+ *
  * We would like to keep them away from declarations for conciseness, which also
  * matches the disposition of non-inline functions. Note that however, unlike C++
  * inline functions, they have to have consistent inline specifiers throughout
