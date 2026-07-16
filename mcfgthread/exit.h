dc8fcd984d279554a08d1550b9b6a644c03664a1
LIU Hao <lh_mouse@126.com>
2026-07-16 20:51:08 +0800

    exit: Use JavaDoc-style comments

--- mcfgthread/exit.h
+++ mcfgthread/exit.h
@@ -16,17 +16,21 @@ __MCF_CXX(extern "C" {)
 #  define __MCF_EXIT_INLINE  __MCF_GNU_INLINE
 #endif
 
-/* Declare 'real' functions here.  */
+/* N1570 ISO/IEC 9899:201x
+ * 7.22.4 Communication with the environment
+ * 7.22.4.5 The _Exit function  */
 __MCF_EXIT_IMPORT __MCF_NEVER_RETURN
 void
 __MCF__Exit(int __status)
   __MCF_noexcept;
 
+/* 7.22.4.7 The quick_exit function  */
 __MCF_EXIT_IMPORT __MCF_NEVER_RETURN
 void
 __MCF_quick_exit(int __status)
   __MCF_noexcept;
 
+/* 7.22.4.4 The exit function  */
 __MCF_EXIT_IMPORT __MCF_NEVER_RETURN
 void
 __MCF_exit(int __status)
