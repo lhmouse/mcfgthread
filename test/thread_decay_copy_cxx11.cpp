354bfb365f7313fe2206b95b6f40cedc0f0b2439
LIU Hao <lh_mouse@126.com>
2026-08-27 18:53:41 +0800

    test: Skip thread_decay_copy_cxx11 when using Microsoft standard library

--- test/thread_decay_copy_cxx11.cpp
+++ test/thread_decay_copy_cxx11.cpp
@@ -2,6 +2,10 @@
  * do whatever you like with this piece of code. Any warranty, explicit
  * or implicit, is disclaimed.  */
 
+#if defined _MSC_VER  // MSVC standard library doesn't compile as C++11.
+int main(void) { return 77;  }
+#else  // MSVC
+
 #include "../mcfgthread/fwd.h"
 
 #if 0 __MCF_CXX11(+1) __MCF_CXX14(+2) != 1
@@ -9,3 +13,5 @@
 #endif
 
 #include "thread_decay_copy.cpp"
+
+#endif  // MSVC
